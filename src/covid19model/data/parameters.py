94e3557617e4d7096cb75f9d99f53eb3d51821d3
JennaVergeynst <jenna.vergeynst@ugent.be>
2020-06-03 16:55:20 +0200

    Remove midcare state (#84)
    * lay-out fix
    
    * remove midcare state
    
    * remove midcare state
    
    * simplify parameter reading and put it in a dictionary
    
    * corrections
    
    * include beta
    
    * correction
    
    * add parameters psi_FP, psi_PP and totalTests
    
    * return missing or redundant parameters in error message + reorder parameters
    
    * remove dmirec and add stratified icu parameter
    
    * add documentation
    
    * only plot mobility data when plot=True
    
    * update notebook for refactored model
    
    * update sciensano data
    
    * update notebook for refactored model
    
    * try-out seirsAge model in notebook

--- src/covid19model/data/parameters.py
+++ src/covid19model/data/parameters.py
@@ -4,22 +4,26 @@ import pandas as pd
 import numpy as np
 
 def get_agemodel_parameters():
-    """Extracts and returns the parameters for the age-stratified deterministic model 
+    """
+    Extracts and returns the parameters for the age-stratified deterministic model
 
-    This function will return all parameters needed to run the age-stratified model. This function was created to group all parameters in one centralised location.
+    This function returns all parameters needed to run the age-stratified model.
+    This function was created to group all parameters in one centralised location.
 
     Returns
     -----------
+    A dictionary with following keys and values:
+
     h : np.array
-        fraction of the cases that require hospitalisation (-) 
+        fraction of the cases that require hospitalisation (-)
     icu: np.array
         fraction of the hospitalized in ICU
     c: np.array
         fraction of the hospitalized in Cohort, calculated as (c = 1 - icu) (-)
     m0: np.array
-        fraction of the patients in ICU who die (-) 
+        fraction of the patients in ICU who die (-)
     a: np.array
-        fraction of asymptomatic cases (-) 
+        fraction of asymptomatic cases (-)
     m: np.array
         fraction of (mild) symptomatic cases (-)
     da: float64
@@ -48,34 +52,28 @@ def get_agemodel_parameters():
 
     Example use
     -----------
-    h,icu,c,m0,a,m,da,dm,dc,dICU,dICUrec,dhospital,beta,sigma,omega,dq = get_agemodel_parameters()
+    parameters = get_agemodel_parameters()
     """
+    # Initialize parameters dictionary
+    parameters = {}
 
     # Verity_etal
-    df=pd.read_csv("../../data/raw/model_parameters/verity_etal.csv", sep=',',header='infer')
-    h =  np.array(df.loc[:,'symptomatic_hospitalized'].astype(float).tolist())/100
-    icu = np.array(df.loc[:,'hospitalized_ICU'].astype(float).tolist())/100
-    c = 1-icu
-    m0 = np.array(df.loc[:,'CFR'].astype(float).tolist())/100/h/icu
+    df = pd.read_csv("../../data/raw/model_parameters/verity_etal.csv", sep=',',header='infer')
+    parameters['h'] =  np.array(df.loc[:,'symptomatic_hospitalized'].astype(float).tolist())/100
+    parameters['icu'] = np.array(df.loc[:,'hospitalized_ICU'].astype(float).tolist())/100
+    parameters['c'] = 1-parameters['icu']
+    parameters['m0'] = np.array(df.loc[:,'CFR'].astype(float).tolist())/100/parameters['h']/parameters['icu']
 
     # Abrams_etal
-    df=pd.read_csv("../../data/raw/model_parameters/abrams_etal.csv", sep=',',header='infer')
-    a =  np.array(df.loc[:,'fraction asymptomatic'].astype(float).tolist())
-    m = 1-a
+    df_asymp = pd.read_csv("../../data/raw/model_parameters/abrams_etal.csv", sep=',',header='infer')
+    parameters['a'] =  np.array(df_asymp.loc[:,'fraction asymptomatic'].astype(float).tolist())
+    parameters['m'] = 1-parameters['a']
 
     # Other parameters
-    df=pd.read_csv("../../data/raw/model_parameters/others.csv", sep=',',header='infer')
-    da = df['da'].values[0]
-    dm = df['dm'].values[0]
-    dc = df['dc'].values[0]
-    dICU = df['dICU'].values[0]
-    dICUrec = df['dICUrec'].values[0]
-    dhospital = df['dhospital'].values[0]
-    sigma = df['sigma'].values[0]
-    omega = df['omega'].values[0]
-    dq = df['dq'].values[0]
+    df_other_pars = pd.read_csv("../../data/raw/model_parameters/others.csv", sep=',',header='infer')
+    parameters.update(df_other_pars.T.to_dict()[0])
 
     # Fitted parameters
-    beta = 0.03492
+    parameters['beta'] = 0.03492
 
-    return h,icu,c,m0,a,m,da,dm,dc,dICU,dICUrec,dhospital,beta,sigma,omega,dq
+    return parameters
--- src/covid19model/models/base.py
+++ src/covid19model/models/base.py
@@ -14,7 +14,18 @@ class BaseModel:
     stratification = None
 
     def __init__(self, states, parameters):
-        """"""
+        """
+        Initialise the models
+
+        Parameters
+        ---------
+        states : dictionary
+            e.g. {'S': N, 'E': np.ones(n_age_classes)} with N total population
+            initialising zeros is not required
+        parameters : dictionary
+            containing the values of all parameters (both stratified and not)
+            these can be obtained with the function parameters.get_agemodel_parameters()
+        """
         self.parameters = parameters
         self.initial_states = states
 
@@ -82,10 +93,15 @@ class BaseModel:
             )
 
         # Validate the params
-        if list(self.parameters.keys()) != specified_params:
+        if set(self.parameters.keys()) != set(specified_params):
             raise ValueError(
-                "The specified parameters don't exactly match the predefined parameters"
+                "The specified parameters don't exactly match the predefined parameters. "
+                "Redundant parameters: {0}. Missing parameters: {1}".format(
+                set(self.parameters.keys()).difference(set(specified_params)),
+                set(specified_params).difference(set(self.parameters.keys())))
             )
+
+        self.parameters = {param: self.parameters[param] for param in specified_params}
 
         # Validate the initial_states / stratified params having the correct length
 
