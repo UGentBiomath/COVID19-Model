b2a5399457ce207c87db690c3f4bc897717b0b48
stijnvanhoey <stijnvanhoey@gmail.com>
2020-06-08 18:16:48 +0200

    Add minimal test on google data format

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,7 +1,10 @@
 
 import pytest
 
+import pandas as pd
+
 from covid19model.data.sciensano import get_sciensano_COVID19_data
+from covid19model.data.google import get_google_mobility_data
 
 def test_sciensano_output():
     # check the characteristics of the sciensano data loda function
@@ -12,3 +15,13 @@ def test_sciensano_output():
     assert isinstance(df.index, pd.DatetimeIndex)
     assert df.index.freq == 'D'
 
+def test_google_output():
+    # check the characteristics of the sciensano data loda function
+    df = get_google_mobility_data(update=False)
+    # set of variable names as output
+    assert set(df.columns) == set(['retail_recreation', 'grocery',
+                                   'parks', 'transport', 'work',
+                                   'residential'])
+    # index is a datetime index with daily frequency
+    assert isinstance(df.index, pd.DatetimeIndex)
+    assert df.index.freq == 'D'
