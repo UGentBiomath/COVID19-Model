700b88066a7ccaa75b5bc038fdce3b603fe1a870
twallema <tijs.alleman@ugent.be>
2020-06-15 18:35:14 +0200

    Visualization aids to utils.py, plt.rcParams.update to init.py

--- src/covid19model/visualization/__init__.py
+++ src/covid19model/visualization/__init__.py
@@ -1,3 +1,17 @@
 from .output import population_status,infected
+from .utils import colorscale_okabe_ito
+import matplotlib.pyplot as plt
 
 __all__ = ["population_status", "infected"]
+
+# covid 19 specific parameters
+plt.rcParams.update({
+    "axes.prop_cycle": plt.cycler('color',
+                                  list(colorscale_okabe_ito.values())),
+    "font.size": 15,
+    "lines.linewidth" : 3,
+    "axes.spines.top": False,
+    "axes.spines.right": False,
+    "ytick.major.left": True,
+    "axes.grid": True
+})
