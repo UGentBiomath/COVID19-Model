b92c9459a6b2657351e14412e66db3e9010a0143
twallema <tijs.alleman@ugent.be>
2020-06-17 12:22:56 +0200

    add compliance to deterministic model

--- src/covid19model/models/models.py
+++ src/covid19model/models/models.py
@@ -116,6 +116,40 @@ class COVID19_SEIRD(BaseModel):
     parameters_stratified_names = ['h', 'c', 'm0', 'icu']
     stratification = 'Nc'
 
+    # logistic compliance model
+    parameters_compliance_names = ['k','t0']
+    #apply_compliance_to = 'Nc' --> abstract further?
+    @staticmethod
+    def compliance(t,Nc_old,Nc_new,k,t0):
+        """
+        A function to simulate tardiness in compliance to social measures
+
+        Parameters
+        ----------
+        t : float or int
+            time since last checkpoint
+        Nc_old : np.array
+            interaction matrix Nc before checkpoint
+        Nc_new : np.array
+            interaction matrix Nc after checkpoint
+        k : float
+            logistic growth steepness of curve
+        t0 : float
+            time after checkpoint at which the logistic curve reaches its sigmoid point
+
+        Returns
+        -------
+        Nc_out : np.array
+            interpolation between Nc_old and Nc_new based on logistic interpolation of each matrix element
+
+        """
+        Nc_out = np.zeros([Nc_old.shape[0],Nc_old.shape[1]])
+        for i in range(Nc_old.shape[0]):
+            for j in range(Nc_old.shape[1]):
+                f = 1/(1+np.exp(-k*(t-t0)))
+                Nc_out[i,j] = Nc_old[i,j] + f*(Nc_new[i,j]-Nc_old[i,j])
+        return Nc_out
+
     # ..transitions/equations
     @staticmethod
     def integrate(t, S, E, I, A, M, C, C_icurec, ICU, R, D, SQ, EQ, IQ, AQ, MQ, RQ,
