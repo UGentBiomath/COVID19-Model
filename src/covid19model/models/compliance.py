ee3216dec3c5ba4d9b7322e52d0f1c603977656c
twallema <tijs.alleman@ugent.be>
2020-06-22 15:50:43 +0200

    implemented one and two parameter ramp compliance functions

--- src/covid19model/models/compliance.py
+++ src/covid19model/models/compliance.py
@@ -39,3 +39,88 @@ def logistic(t,old,new,k,t0):
             for j in range(n2):
                 out[i,j] = old[i,j] + f*(new[i,j]-old[i,j])
         return out
+
+def ramp_1(t,old,new,l):
+        """
+        A function to simulate tardiness in compliance to social measures.
+
+        Interpolates a parameter between the values 'old' and 'new' using a one parameter ramp function.
+
+        Parameters
+        ----------
+        t : float or int
+            time since last checkpoint
+        old : np.array
+            parameter value before checkpoint
+        new : np.array
+            parameter value after checkpoint
+        l : float
+            time to reach full compliance
+
+        Returns
+        -------
+        out : np.array
+            interpolation between old and new parameter value
+        """
+
+        if isinstance(old,int) or isinstance(old,int) or isinstance(new,int) or isinstance(old,int):
+            n1 = 1
+            n2 = 1
+        else:
+            n1 = old.shape[0]
+            n2 = old.shape[1]
+        # perform interpolation
+        if t <= l:
+            f = (1/l)*t
+        else:
+            f = 1.0
+        out = np.zeros([n1,n2])
+        for i in range(n1):
+            for j in range(n2):
+                out[i,j] = old[i,j] + f*(new[i,j]-old[i,j])
+        return out
+
+def ramp_2(t,old,new,l,tau):
+        """
+        A function to simulate tardiness in compliance to social measures.
+
+        Interpolates a parameter between the values 'old' and 'new' using a two parameter ramp function.
+
+        Parameters
+        ----------
+        t : float or int
+            time since last checkpoint
+        old : np.array
+            parameter value before checkpoint
+        new : np.array
+            parameter value after checkpoint
+        l : float
+            time to reach full compliance
+        tau: float
+            time delay before ramp starts
+            
+        Returns
+        -------
+        out : np.array
+            interpolation between old and new parameter value
+
+        """
+
+        if isinstance(old,int) or isinstance(old,int) or isinstance(new,int) or isinstance(old,int):
+            n1 = 1
+            n2 = 1
+        else:
+            n1 = old.shape[0]
+            n2 = old.shape[1]
+        # perform interpolation
+        if t <= tau:
+            f = 0.0
+        elif tau < t <= tau+l:
+            f = (1/l)*t - (1/l)*tau
+        else:
+            f = 1.0
+        out = np.zeros([n1,n2])
+        for i in range(n1):
+            for j in range(n2):
+                out[i,j] = old[i,j] + f*(new[i,j]-old[i,j])
+        return out
