6b34901a24360d9e9f0444925c7b5a554f70a8ab
twallema <tijs.alleman@ugent.be>
2020-06-29 17:56:24 +0200

    changes to visualisation of optimization functions

--- src/covid19model/visualization/optimization.py
+++ src/covid19model/visualization/optimization.py
@@ -8,7 +8,7 @@ import numpy as np
 from .utils import colorscale_okabe_ito
 from .output import _apply_tick_locator
 
-def traceplot(samples,labels,plt_kwargs={}):
+def traceplot(samples,labels,plt_kwargs={},filename=None):
     """Make a visualization of sampled parameters
 
     Parameters
@@ -35,6 +35,8 @@ def traceplot(samples,labels,plt_kwargs={}):
         )
     # initialise figure
     fig, axes = plt.subplots(len(labels))
+    # set size
+    fig.set_size_inches(10, len(labels)*7/3)
     # plot data
     for i in range(ndim):
         ax = axes[i]
@@ -43,9 +45,13 @@ def traceplot(samples,labels,plt_kwargs={}):
         ax.set_ylabel(labels[i])
     axes[-1].set_xlabel("step number")
 
+    if filename:
+        plt.savefig(filename, dpi=600, bbox_inches='tight',
+                    orientation='portrait', papertype='a4')
+
     return ax
 
-def plot_fit(y_model,data,start_date,lag_time,states,T=1,data_mkr=['o','v','s','*','^'],clr=['green','orange','red','black','blue'],
+def plot_fit(y_model,data,start_date,lag_time,states,T=1,data_mkr=['o','v','s','*','^'],
                 legend_text=None,titleText=None,ax=None,plt_kwargs={},sct_kwargs={}):
 
     """Plot model fit to user provided data 
@@ -99,10 +105,10 @@ def plot_fit(y_model,data,start_date,lag_time,states,T=1,data_mkr=['o','v','s','
     y_model = y_model.sum(dim="stratification")
     for i in range(len(data)):
         data2plot = y_model[states[i]].to_array(dim="states").values.ravel()
-        lines = ax.plot(idx,data2plot,color=clr[i],**plt_kwargs)    
+        lines = ax.plot(idx,data2plot,**plt_kwargs)    
     # Plot data
     for i in range(len(data)):
-        lines=ax.scatter(idx[lag_time:-T],data[i],color="black",marker=data_mkr[i],**sct_kwargs)
+        lines=ax.scatter(idx[lag_time:-T],data[i],color="black",**sct_kwargs)
 
     # Attributes
     if legend_text is not None:
