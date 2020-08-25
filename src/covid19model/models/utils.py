93d4f1d81abf60bf3a9fb7aac62a64ad1b190d6e
twallema <tijs.alleman@ugent.be>
2020-08-25 16:36:58 +0200

    add function to covert name into NIS code

--- src/covid19model/models/utils.py
+++ src/covid19model/models/utils.py
@@ -1,6 +1,43 @@
 import numpy as np 
+import pandas as pd 
 
 def sample_beta_binomial(n, p, k, size=None):
     p = np.random.beta(k/(1-p), k/p, size=size)
     r = np.random.binomial(n, p)
     return r
+
+def name2nis(name):
+    """
+    A function to convert the name of a Belgian municipality/arrondissement/province/etc. to its NIS code
+
+    Parameters
+    ----------
+    name : str
+        the name of the municipality/arrondissement/province/etc.
+
+    Returns
+    -------
+    NIS : float
+        the NIS code corresponding with the given name
+
+    """
+    # Load the list of name-NIS couples
+    name_df=pd.read_pickle('../../data/interim/census_2011/NIS_name.pkl')
+    pos_name = name_df['name'].values
+    # Convert list of possible names to lowercase only
+    pos_name_lower = [string.lower() for string in pos_name]
+    name_df['name'] = pos_name_lower
+    # Check if input is a string
+    if not isinstance(name,str):
+        raise TypeError(
+            "name2nis input must be a string"
+            )
+    # Convert input to lowercase
+    name = name.lower()
+    # Search for a match and return NIS code
+    if not name in pos_name_lower:
+        raise ValueError(
+                "No match for '{0}' found".format(name)
+            )
+    else:
+        return name_df[name_df['name'] == name]['NIS'].values[0]
