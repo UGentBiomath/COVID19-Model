45b2585e9400feb4ca8da8727729240ac225afe8
twallema <tijs.alleman@ugent.be>
2020-08-25 11:27:01 +0200

    added loading of rec. mobility mtx to parameter loading function
    also general tweaks to the function, update of docstring etc

--- src/covid19model/data/model_parameters.py
+++ src/covid19model/data/model_parameters.py
@@ -4,60 +4,51 @@ import pandas as pd
 import numpy as np
 from covid19model.data import polymod
 
-def get_COVID19_SEIRD_parameters(stratified=True):
+def get_COVID19_SEIRD_parameters(age_stratified=True, spatial=False):
     """
-    Extracts and returns the parameters for the age-stratified deterministic model
+    Extracts and returns the parameters for the age-stratified deterministic model (spatial or non-spatial)
 
     This function returns all parameters needed to run the age-stratified model.
     This function was created to group all parameters in one centralised location.
 
     Parameters
     ----------
+
     stratified : boolean
         If True: returns parameters stratified by age, for agestructured model
         If False: returns parameters for non-agestructured model
 
     Returns
-    -----------
-    A dictionary with following keys and values:
+    -------
 
-    Nc: np.array
-        9x9 social interaction matrix; by default, the total interaction
-        matrix Nc_total from the Polymod study is assigned to the parameters dictionary
-    s : np.array
-        relative susceptibility per age group (-)
-    h : np.array
-        fraction of the cases that require hospitalisation (-)
-    icu: np.array
-        fraction of the hospitalized in ICU
-    c: np.array
-        fraction of the hospitalized in Cohort, calculated as (c = 1 - icu) (-)
-    m0: np.array
-        fraction of the patients in ICU who die (-)
-    a: np.array
-        fraction of asymptomatic cases (-)
-    m: np.array
-        fraction of (mild) symptomatic cases (-)
-    da: float64
-        length of infectiousness in case of asymptomatic infection (days)
-    dm: float64
-        length of infectiousness in case of mild symptomatic infection (days)
-    dc: float64
-        length of stay in Cohort when symptoms don't worsen (days)
-    dICU: float64
-        length of stay in ICU (days)
-    dICUrec: float64
-        length of recovery stay in Cohort after stay in ICU (days)
-    dhospital: float64
-        time between first symptom and hospitalization (days)
-    beta: float64
-        chance of transmission when coming into contact with an infectious person (-)
-    sigma: float64
-        length of latent period (days)
-    omega: float64
-        length of pre-symptomatic infectious period (days)
-    dq: float64
-        length of quarantine when patient does not develop symptoms (days)
+    pars_dict : dictionary
+        containing the values of all parameters (both stratified and not)
+        these can be obtained with the function parameters.get_COVID19_SEIRD_parameters()
+
+        Non-stratified parameters
+        -------------------------
+        beta : probability of infection when encountering an infected person
+        sigma : length of the latent period
+        omega : length of the pre-symptomatic infectious period
+        zeta : effect of re-susceptibility and seasonality
+        a : probability of an asymptomatic cases
+        m : probability of an initially mild infection (m=1-a)
+        da : duration of the infection in case of asymptomatic
+        dm : duration of the infection in case of mild
+        der : duration of stay in emergency room/buffer ward
+        dc : average length of a hospital stay when not in ICU
+        dICU_R : average length of a hospital stay in ICU in case of recovery
+        dICU_D: average length of a hospital stay in ICU in case of death
+        dhospital : time before a patient reaches the hospital
+
+        Age-stratified parameters
+        --------------------
+        s: relative susceptibility to infection
+        a : probability of a subclinical infection
+        h : probability of hospitalisation for a mild infection
+        c : probability of hospitalisation in Cohort (non-ICU)
+        m_C : mortality in Cohort
+        m_ICU : mortality in ICU
 
     Example use
     -----------
@@ -71,7 +62,7 @@ def get_COVID19_SEIRD_parameters(stratified=True):
     # Initialize parameters dictionary
     pars_dict = {}
 
-    if stratified == True:
+    if age_stratified == True:
 
         # Assign Nc_total from the Polymod study to the parameters dictionary
         Nc_total = polymod.get_interaction_matrices()[-1]
@@ -80,8 +71,8 @@ def get_COVID19_SEIRD_parameters(stratified=True):
         # Assign AZMM and UZG estimates to correct variables
         df = pd.read_csv(os.path.join(par_interim_path,"AZMM_UZG_hospital_parameters.csv"), sep=',',header='infer')
         pars_dict['c'] = np.array(df['c'].values[:-1])
-        pars_dict['m0_C'] = np.array(df['m0_{C}'].values[:-1])
-        pars_dict['m0_ICU'] = np.array(df['m0_{ICU}'].values[:-1])
+        pars_dict['m_C'] = np.array(df['m0_{C}'].values[:-1])
+        pars_dict['m_ICU'] = np.array(df['m0_{ICU}'].values[:-1])
         pars_dict['dc_R'] = np.array(df['dC_R'].values[-1])
         pars_dict['dc_D'] = np.array(df['dC_D'].values[-1])
         pars_dict['dICU_R'] = np.array(df['dICU_R'].values[-1])
@@ -91,10 +82,6 @@ def get_COVID19_SEIRD_parameters(stratified=True):
         # verity_etal
         df = pd.read_csv(os.path.join(par_raw_path,"verity_etal.csv"), sep=',',header='infer')
         pars_dict['h'] =  np.array(df.loc[:,'symptomatic_hospitalized'].astype(float).tolist())/100
-
-        # molenberghs_etal
-        #df = pd.read_csv(os.path.join(par_raw_path,"molenberghs_etal.csv"), sep=',',header='infer')
-        #pars_dict['m0'] = np.array(df.loc[:,'IFR_general_population'].astype(float).tolist())/100/pars_dict['h']
 
         # davies_etal
         df_asymp = pd.read_csv(os.path.join(par_raw_path,"davies_etal.csv"), sep=',',header='infer')
@@ -106,6 +93,15 @@ def get_COVID19_SEIRD_parameters(stratified=True):
 
         non_strat = pd.read_csv(os.path.join(par_raw_path,"non_stratified.csv"), sep=',',header='infer')
         pars_dict.update({key: np.array(value) for key, value in non_strat.to_dict(orient='list').items()})
+
+    # Add recurrent mobility matrix to parameter dictionary
+    if spatial == True:
+        mobility_df=pd.read_pickle('../../data/interim/census_2011/recurrent_mobility_arrondissements.pkl')
+        NIS=mobility_df.values
+        # Normalize recurrent mobility matrix
+        for i in range(NIS.shape[0]):
+            NIS[i,:]=NIS[i,:]/sum(NIS[i,:])
+        pars_dict.update({'NIS': NIS})
 
     # Other parameters
     df_other_pars = pd.read_csv(os.path.join(par_raw_path,"others.csv"), sep=',',header='infer')
