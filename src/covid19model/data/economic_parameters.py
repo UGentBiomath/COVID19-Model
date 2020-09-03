62019007a0b33340f9861ace6a1cf8fb0017f55f
twallema <tijs.alleman@ugent.be>
2020-09-03 13:27:36 +0200

    finished function to load interim data

--- src/covid19model/data/economic_parameters.py
+++ src/covid19model/data/economic_parameters.py
@@ -38,4 +38,23 @@ def get_economic_parameters():
     # Initialize parameters dictionary
     pars_dict = {}
 
+    # IO_NACE64.csv
+    df = pd.read_csv(os.path.join(par_interim_path,"IO_NACE64.csv"), sep=',',header=[0],index_col=[0])
+    pars_dict['IO'] = df.values
+
+    # Others.csv
+    df = pd.read_csv(os.path.join(par_interim_path,"others.csv"), sep=',',header=[1],index_col=[0])
+    pars_dict['x_0'] = np.expand_dims(np.array(df['Sectoral output (M€)'].values), axis=1)
+    pars_dict['c_0'] = np.expand_dims(np.array(df['Household demand (M€)'].values), axis=1)
+    pars_dict['f_0'] = np.expand_dims(np.array(df['Other demand (M€)'].values), axis=1)
+    pars_dict['n'] = np.expand_dims(np.array(df['Desired stock (days)'].values), axis=1)
+    pars_dict['c_s'] = np.expand_dims(np.array(df['Consumer demand shock (%)'].values), axis=1)
+    pars_dict['f_s'] = np.expand_dims(np.array(df['Other demand shock (%)'].values), axis=1)
+    pars_dict['l_0'] = np.expand_dims(np.array(df['Employees (x1000)'].values), axis=1)*1000
+    pars_dict['l_s'] = np.expand_dims(np.array(df['Employees (x1000)'].values), axis=1)*1000*np.expand_dims(np.array((df['Telework (%)'].values+df['Mix (%)'].values+df['Workplace (%)'].values)/100), axis = 1)
+
+    # IHS_critical_NACE64.csv
+    df = pd.read_csv(os.path.join(par_interim_path,"IHS_critical_NACE64.csv"), sep=',',header=[0],index_col=[0])
+    pars_dict['C'] = df.values
+
     return pars_dict
