5a3c495194d5a5b9e3b686c2680b029cfb09c5cb
twallema <tijs.alleman@ugent.be>
2020-09-23 13:01:11 +0200

    remove 'apply compliance to' in stochastic model

--- src/covid19model/models/models.py
+++ src/covid19model/models/models.py
@@ -175,7 +175,6 @@ class COVID19_SEIRD_sto(BaseModel):
     parameter_names = ['beta', 'sigma', 'omega', 'zeta', 'da', 'dm', 'der', 'dc_R', 'dc_D', 'dICU_R', 'dICU_D', 'dICUrec', 'dhospital']
     parameters_stratified_names = [['s','a','h', 'c', 'm_C','m_ICU']]
     stratification = ['Nc']
-    apply_compliance_to = 'Nc'
 
     # ..transitions/equations
     @staticmethod
