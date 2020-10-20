c72b6ad461996ab173dcd6ef7fec0a911a546088
mrollier <michiel.rollier@ugent.be>
2020-10-20 17:36:16 +0200

    Create function `show_map` in `visualization` for spatial simulation
    Plot a snapshot of the median evolution of the infection on a Geopandas map, with the possiblity of also showing time series on the side.
    
    Handy tool for quick visualisation of the results of spatial simulations.

--- src/covid19model/visualization/output.py
+++ src/covid19model/visualization/output.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec # subfigure structuring
+from mpl_toolkits.axes_grid1 import make_axes_locatable # for plot aesthetics
+import matplotlib.colors as colors
 from .utils import colorscale_okabe_ito
 from .utils import _apply_tick_locator
 
@@ -152,3 +155,238 @@ def infected(data, asymptomatic=False, mild=False, filename=None, *, ax=None, **
         plt.savefig(filename, dpi=600, bbox_inches='tight')
 
     return lines
+
+
+def show_map(data, geo, ts_geo='E', day=0, lin=False, rel=False, cmap='Oranges', nis=None,
+                 ts_graph=['E', 'H_in', 'ICU', 'D'], figname=None, dpi=200, verbose=False):
+    """Plot a snapshot of the median evolution of the infection on a Geopandas map
+    
+    Parameters
+    ----------
+    data : xarray DataSet
+        Model output. 'place' dimension values are integers.
+    geo : GeoDataFrame
+        Geopandas dataframe from Belgian shapefiles whose entries correspond to the values in the model output's 'place' dimension. NISCode values are strings.
+    ts_geo : string
+        The SEIR compartment time series that is plotted into the color map on the chosen day. Either S, E (default), I, A, M, ER, C, C_icurec, ICU, R, D, H_in, H_out, or H_tot.
+    day : int
+        The simulated day that is to be plotted in the color map. Iterate over this value to create an animation. Default is start date.
+    lin : Boolean
+        Plots a linear representation of the values in the map if True. Otherwise the values are shown with a symlog representation (default).
+    rel : Boolean
+        Plot the values relative to the population if True. Default is False: absolute numbers.
+    cmap : string
+        The color used in the color map of the Geopandas plot. Default is 'Oranges'. Best to use sequential coloring.
+    nis : int or array of int
+        NIS value or array of NIS values of the region(s) whose time series will appear in the graphs. Maximum number of graphs per plot is 6, as showing too many will make the graph illegible. Default is None: sum of all regions.
+    ts_graph : string or list of strings or None
+        Choose which time series are to be shown in the graphs on the side of the map. List contains one or more of S, E (default), I, A, M, ER, C, C_icurec, ICU, R, D, H_in, H_out, or H_tot. Default: ['E', 'H_in', 'ICU', 'D']. Maximal length is 5. If None is chosen only the geopandas map is shown
+    figname : string
+        Directory and name for the figure when saved. Include data extension type (e.g. .jpg). Default is None (image is not saved). Make sure to add an iterator name when iterating over days, in order not to overwrite images.
+    dpi : int
+        Resolution of the saved image. Only applicable if figname is not None.
+    verbose : Boolean
+        Print progress and additional information. Default is False.
+        
+    Returns
+    -------
+    maps : AxesSubplot
+        Plot of the geopandas color map. The user can customize this map further.
+    graphs : array of AxesSubplot objects (optional)
+        Plots of the entire time series
+        
+    TO DO
+    -----
+    Implement rel=True for relative values (import population per NIS value, change vmax value and method to show the sum of results)
+    """
+    ####################
+    # CHECK PARAMETERS #
+    ####################
+    
+    # Raise exceptions in case of wrong input
+    # Verify that NIS values correspond
+    nis_geo = geo.NISCode.values.astype(int)
+    nis_data = data.place.values.astype(int)
+    if set(nis_geo) != set(nis_data):
+        raise Exception(f"The NIS values in the model output and the geopandas dataframe do not correspond (function parameters 'data' and 'geo')")
+        
+    # Check whether the model output has recognisable compartments
+    full_comp_set = {'S', 'E', 'I', 'A', 'M', 'ER', 'C', 'C_icurec', 'ICU', 'R', 'D', 'H_in', 'H_out', 'H_tot'}
+    data_comp_set = set(data.data_vars)
+    if not data_comp_set.issubset(full_comp_set):
+        diff = data_comp_set.difference(full_comp_set)
+        raise Exception(f"Unrecognised compartments in data: {diff}")
+    if ts_geo not in data_comp_set:
+        raise Exception(f"'{ts_geo}' is not an acceptable compartment time series to plot in a map. Choose a ts_geo value from {data_comp_set}")
+    if verbose:
+        print(f"Compartments whose time series are included in the data: {data_comp_set}")
+    
+    # Check if the day is not out of reach
+    maxday = data.time.values[-1]
+    if (day < 0) or (day > maxday) or (type(day) != int):
+        raise Exception(f"Choose an integer value day between (and including) 0 and {maxday}")
+    if verbose:
+        print(f"Working on day {day} of {maxday}")
+    
+    # Check if the chosen nis value (if any) is legitimate
+    if nis:
+        if type(nis) != list:
+            nis = [nis]
+        for value in nis:
+            if type(value) != int:
+                raise Exception(f"The chosen NIS value(s) must be (an) integer(s)")
+        if not set(nis).issubset(nis_geo):
+            raise Exception(f"The chosen NIS value(s) to plot in the graphs do(es) not correspond to data (parameter 'nis')")
+        if len(nis) > 6:
+            raise Exception(f"List of NIS values is longer than 6; showing more than 6 graphs per plot turns it illegible")
+    
+    # Check if the list of compartment whose time series are to be plotted, is in the correct format
+    if ts_graph:
+        if type(ts_graph) != list:
+            ts_graph = [ts_graph]
+        for ts in ts_graph:
+            if type(ts) != str:
+                raise Exception(f"Parameter 'ts_graph', the chosen compartment(s) for time series graphing, must be a (list of) string(s) from {data_comp_set}")
+            if ts not in data_comp_set:
+                raise Exception(f"'{ts}' is not an acceptable compartment time series to graph. Choose for parameter 'ts_graph' a (list of) string value(s) from {data_comp_set}, corresponding to the time series that are to be graphed")
+            
+    # Check properties of figname and dpi
+    if (figname != None) & (type(figname) != str):
+        raise Exception(f"Parameter 'figname' should be a string with the appropriate data extension (.jpg, .png, .svg, .pdf, ...)")
+    if (type(dpi) != int) or (dpi < 1):
+        raise Exception(f"Parameter 'dpi' should be an integer larger than 0.")
+
+    #############################################
+    # INITIATE INTERNAL VALUES AND ENVIRONMENTS #
+    #############################################
+    
+    # Define time list from data
+    tlist = data['time'].values
+    
+    # Define colors dictionary for graphs (maximally six)
+    if ts_graph:
+        color_list_total = ['g', 'orange', 'r', 'k', 'c', 'm']
+        color_dict_total = dict(zip(ts_graph, color_list_total[:len(ts_graph)]))
+    if nis:
+        color_list = ['b', 'g', 'r', 'c', 'm', 'y']
+        color_dict = dict(zip(nis, color_list[:len(nis)]))
+    
+    # Initialise new column in geopandas dataframe containing time series value of the chosen compartment
+    geo.loc[:,ts_geo] = 0
+    
+    # Set minima/maxima dictionary based on median value
+    # Note that the age classes are aggregated (this may be extended later)
+    if ts_graph:
+        vmax_graph = dict({})
+        for ts in ts_graph:
+            vmax_graph[ts]=0
+            if nis: # take highest value of all places
+                for nis_value in nis:
+                    vmax_temp = data[ts].sum(dim='Nc').sel(place=nis_value).quantile(0.5, dim='draws').values.max()
+                    if vmax_temp > vmax_graph[ts]:
+                        vmax_graph[ts] = vmax_temp
+            else: # Take highest value of the sum of all places (works only for absolute numbers right now!
+                vmax_graph[ts] = data[ts].sum(dim='Nc').sum(dim='place').quantile(0.5, dim='draws').values.max()
+            if verbose:
+                print(f"Maximum value of median draw for timeseries of compartment {ts} (vmax_graph[{ts}]) set to {vmax_graph[ts]}.")
+        vmin_graph = 0
+    
+    vmax_geo = data[ts_geo].sum(dim='Nc').quantile(0.5, dim='draws').values.max()
+    vmin_geo = 0
+    
+    # Initiate plotting environment
+    #if we are plotting graphs on the righthand side
+    if ts_graph:
+        fig = plt.figure(figsize=(15,7))
+        gs = fig.add_gridspec(len(ts_graph), 2, width_ratios=[2,1])
+        cax_side = 'left'
+    #if we are only plotting the map
+    else:
+        fig = plt.figure(figsize=(15,10))
+        gs = fig.add_gridspec(1,1)
+        cax_side = 'right'
+        
+    # Axes for Geopandas
+    ax0 = fig.add_subplot(gs[:, 0])
+    ax0.set_axis_off()
+    cax = make_axes_locatable(ax0).append_axes(cax_side, size="5%", pad=1) # Legend properties
+
+    # Axes for graphs
+    text_size=18
+    #ylabel_pos = (-0.12,0.5)
+    yscale_graph = 'linear'
+    if ts_graph:
+        pos = 0
+        ax_graph_dict = dict({})
+        for ts in ts_graph:
+            ax_graph_dict[ts] = fig.add_subplot(gs[pos,1])
+            ax_graph_dict[ts].set_ylabel(ts, size=text_size)
+            #ax_graph_dict[ts].yaxis.set_label_coords(ylabel_pos[0], ylabel_pos[1])
+            ax_graph_dict[ts].set_yscale(yscale_graph)
+            ax_graph_dict[ts].grid(False)
+            ax_graph_dict[ts].set_xlim([0,tlist[-1]])
+            ax_graph_dict[ts].set_ylim([vmin_graph,1.25*vmax_graph[ts]])
+            if pos != (len(ts_graph)-1):
+                ax_graph_dict[ts].set_xticks([])
+            pos += 1
+        ax_graph_dict[ts].set_xlabel('Days since initial exposure',size=text_size)
+    
+    # Set percentage edges to plot
+    upper_pct = 90
+    lower_pct = 100 - upper_pct
+    
+    ########################
+    # Import and plot data #
+    ########################
+    
+    # Add data to geopandas dataframe
+    for nis_value in nis_data:
+        ts_median_today = data.sel(place=nis_value, time=day).sum(dim='Nc').quantile(0.5, dim='draws')[ts_geo].values
+        geo.loc[geo['NISCode']==str(nis_value), ts_geo] = ts_median_today
+    if lin:
+        output = geo.plot(column=ts_geo, ax=ax0, cmap=cmap, legend=True, edgecolor = 'k',
+                 vmin=vmin_geo, vmax=vmax_geo, cax=cax, alpha=1)
+    else:
+        output = geo.plot(column=ts_geo, ax=ax0, cmap=cmap,legend=True, edgecolor = 'k',
+                 norm=colors.SymLogNorm(linthresh=1, vmin=vmin_geo, vmax=vmax_geo), cax=cax, alpha=1)
+
+    # Add (meta)data to graphs
+    legend_size = 12
+    figtext_pos = (.18, .12)
+    if ts_graph:
+        for ts in ts_graph:
+            if nis:
+                for nis_value in nis:
+                    ts_median = data[ts].sel(place=nis_value).sum(dim='Nc').quantile(0.5, dim='draws').values
+                    ts_lower = data[ts].sel(place=nis_value).sum(dim='Nc').quantile(lower_pct/100, dim='draws').values
+                    ts_upper = data[ts].sel(place=nis_value).sum(dim='Nc').quantile(upper_pct/100, dim='draws').values
+                    label = str(nis_value)
+                    if len(nis) > 1:
+                        ax_graph_dict[ts].plot(tlist, ts_median, color=color_dict[nis_value], alpha=1, linewidth=2,label=label)
+                        ax_graph_dict[ts].fill_between(tlist, ts_lower, ts_upper, color=color_dict[nis_value], alpha=0.3)
+                    else:
+                        ax_graph_dict[ts].plot(tlist, ts_median, color=color_dict_total[ts], alpha=1, linewidth=2,label=label)
+                        ax_graph_dict[ts].fill_between(tlist, ts_lower, ts_upper, color=color_dict_total[ts], alpha=0.3)
+            else:
+                ts_median = data[ts].sum(dim='place').sum(dim='Nc').quantile(0.5, dim='draws').values
+                ts_lower = data[ts].sum(dim='place').sum(dim='Nc').quantile(lower_pct/100, dim='draws').values
+                ts_upper = data[ts].sum(dim='place').sum(dim='Nc').quantile(upper_pct/100, dim='draws').values
+                label='national'
+                ax_graph_dict[ts].plot(tlist, ts_median, color=color_dict_total[ts], alpha=1, linewidth=2,label=label)
+                ax_graph_dict[ts].fill_between(tlist, ts_lower, ts_upper, color=color_dict_total[ts], alpha=0.3)
+            if (nis and (len(nis) == 1)) or not nis:
+                ax_graph_dict[ts].legend(loc=2, prop={'size':legend_size})
+            ax_graph_dict[ts].axvline(day, color='r', linewidth=2, linestyle='--')
+        if nis and (len(nis) > 3):
+            ax_graph_dict[ts].legend(loc=2, prop={'size':legend_size}, ncol=2)
+        else:
+            ax_graph_dict[ts].legend(loc=2, prop={'size':legend_size}, ncol=1)
+        plt.figtext(figtext_pos[0], figtext_pos[1], f"People in compartment {ts_geo} at day {day}", backgroundcolor='whitesmoke', fontsize=18)
+    else:
+        plt.figtext(.12, .2, f"People in compartment {ts_geo} at day {day}", backgroundcolor='whitesmoke', fontsize=22)
+
+    # Save figure
+    if figname:
+        plt.savefig(figname, dpi=dpi, bbox_inches='tight')
+        print(f"Saved figure '{figname}'")
+        plt.close('all')
