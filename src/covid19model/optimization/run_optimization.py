339918d88f209284e2d0170e1e74b01bf8307bb5
jvergeyn <jvergeyn@eraser1.ugent.be>
2020-11-06 08:45:01 +0100

    run with pso limited to warmup 60

--- src/covid19model/optimization/run_optimization.py
+++ src/covid19model/optimization/run_optimization.py
@@ -100,7 +100,7 @@ def full_calibration_wave1(model, timeseries, spatial_unit, start_date, end_beta
     #############################################
     # set optimisation settings
     parNames_pso = ['sigma_data','extraTime','beta'] # must be a list!
-    bounds_pso=((1,100),(30,100),(0.01,0.06)) # must be a list!
+    bounds_pso=((1,100),(30,60),(0.01,0.06)) # must be a list!
     if pso==True:
     # run pso optimisation
         theta = MCMC.fit_pso(model,data,parNames_pso,states,bounds_pso,maxiter=maxiter,popsize=popsize,
