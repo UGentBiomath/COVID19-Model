b432f6247075d314c0ce6166ec34ad53b54f5f46
Michiel Rollier <Michiel.Rollier@UGent.be>
2020-12-08 21:12:39 +0100

    Add option to drop nan values in the return as a result of the moving average

--- src/covid19model/models/utils.py
+++ src/covid19model/models/utils.py
@@ -355,7 +355,7 @@ def open_sim(name, group, verbose=True):
     return out
 
 
-def moving_avg(timeseries, days=7, win_type=None, params=None):
+def moving_avg(timeseries, days=7, dropna=False, win_type=None, params=None):
     """Takes a centred moving average of a time series over a window with user-defined width and shape. Note: when taking a N-day centred moving average, the first and last N//2 days won't return a value and are effectively lost, BUT they are returned as nans. Also note that the moving average applied over missing data will result in an effectively larger window - try to make sure there is no missing data in the range the data is averaged.
     
     Parameters
@@ -364,6 +364,8 @@ def moving_avg(timeseries, days=7, win_type=None, params=None):
         Pandas DataFrame with chronologically ordered datetime objects as indices and a single column with float values
     days : int
         Width of the averaging window in days. Resulting value is recorded in the centre of the window. 7 days by default. If the number is uneven, the window is asymmetric toward the past.
+    dropnan : boolean
+        If True: only return dates that have an averaged value - i.e. drop some values at the start and the end.
     win_type : str
         Type of window, determining the weight of the values in the window. Choice between 'boxcar', 'triang', 'blackman', 'hamming', 'bartlett', 'parzen', 'bohman', 'blackmanharris', 'nuttall', 'barthann', 'kaiser' (needs parameter: beta), 'gaussian' (needs parameter: std), 'general_gaussian' (needs parameters: power, width), 'slepian' (needs parameter: width), 'exponential' (needs parameter: tau). If win_type=None (default) all points are evenly weighted.
     params : float or list of floats
@@ -395,4 +397,6 @@ def moving_avg(timeseries, days=7, win_type=None, params=None):
         ts_temp = ts_temp.rolling(window=days, center=True, win_type=win_type).mean(tau=params[0])
     
     timeseries_avg = pd.DataFrame(data=ts_temp.values.copy(), index=timeseries.index.copy(), columns=[col_name])
+    if dropna:
+        timeseries_avg.dropna(inplace=True)
     return timeseries_avg
