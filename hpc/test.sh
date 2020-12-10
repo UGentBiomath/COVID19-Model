4c1b2cc5239175dfcaad2897bca25cea74821242
twallema <tijs.alleman@ugent.be>
2020-12-10 14:13:52 +0100

    remove legacy code from test job script

--- hpc/test.sh
+++ hpc/test.sh
@@ -3,10 +3,6 @@
 #PBS -l nodes=1:ppn=all ## single-node job, all available cores
 #PBS -l walltime=72:00:00 ## max. 72h of wall time
 
-# load python modules (legacy)
-#module load networkx/2.4-intel-2019b-Python-3.7.4
-#module load matplotlib/3.1.1-intel-2019b-Python-3.7.4
-
 # Change to package folder
 cd $VSC_HOME/Documents/COVID19-Model/hpc/
 
