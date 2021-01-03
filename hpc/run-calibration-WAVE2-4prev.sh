473fae01fbac19e4c8cfe5e03f76703742243304
twallema <tijs.alleman@ugent.be>
2021-01-03 12:49:00 +0100

    change name of test.sh to more descriptive name

--- hpc/run-calibration-WAVE2-4prev.sh
+++ hpc/run-calibration-WAVE2-4prev.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #PBS -N calibration-COVID19-SEIRD-WAVE2 ## job name
-#PBS -l nodes=1:ppn=all ## single-node job, all available cores
+#PBS -l nodes=1:ppn=9 ## single-node job, all available cores
 #PBS -l walltime=72:00:00 ## max. 72h of wall time
 
 # Change to package folder
 cd $VSC_HOME/Documents/COVID19-Model/hpc/
 
 # Make script executable
-chmod +x twallema-calibration-WAVE2-3prev.py
+chmod +x twallema-calibration-WAVE2-4prev.py
 
 # Activate conda environment
 source activate COVID_MODEL
 
 # Execute script
-python twallema-calibration-WAVE2-3prev.py
+python twallema-calibration-WAVE2-4prev.py
 
 # Deactivate environment
 conda deactivate
