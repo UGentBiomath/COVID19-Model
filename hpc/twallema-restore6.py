989de7429964e96fd2c1429f9ac6e98b27d59d36
twallema <tijs.alleman@ugent.be>
2021-01-05 13:40:25 +0100

    bugs in restore6 script

--- hpc/twallema-restore6.py
+++ hpc/twallema-restore6.py
@@ -38,8 +38,8 @@ report_version = '6.0'
 end_sim = '2021-05-01'
 start_sim = '2020-09-01'
 model = 'BIOMATH COVID-19 SEIRD national'
-n_samples = 500
-n_draws_per_sample = 5000
+n_samples = 100
+n_draws_per_sample = 100
 warmup = 0
 conf_int = 0.05
 # Upper- and lower confidence level
@@ -92,9 +92,9 @@ with open('../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/
 
 # Extract build contact matrix function
 from covid19model.models.time_dependant_parameter_fncs import make_contact_matrix_function
-contact_matrix_4prev, contact_matrix_3prev = make_contact_matrix_function(df_google, Nc_all)
+contact_matrix_4prev = make_contact_matrix_function(df_google, Nc_all)
 
-def report6_policy_function(t, param, l , tau, prev_schools, prev_work, prev_rest,scenario='1a'):
+def report6_policy_function(t, param, l , tau, prev_home, prev_schools, prev_work, prev_rest,scenario='1a'):
     # Convert tau and l to dates
     tau_days = pd.Timedelta(tau, unit='D')
     l_days = pd.Timedelta(l, unit='D')
@@ -267,6 +267,7 @@ def draw_fcn(param_dict,samples_dict,to_sample):
     idx, param_dict['beta'] = random.choice(list(enumerate(samples_dict['beta'])))
     param_dict['l'] = samples_dict['l'][idx] 
     param_dict['tau'] = samples_dict['tau'][idx]    
+    param_dict['prev_home'] = samples_dict['prev_home'][idx] 
     param_dict['prev_schools'] = samples_dict['prev_schools'][idx]    
     param_dict['prev_work'] = samples_dict['prev_work'][idx]       
     param_dict['prev_rest'] = samples_dict['prev_rest'][idx] 
@@ -287,6 +288,7 @@ for scenario in scenarios:
     # Add the time-dependant parameter function arguments
     params.update({'l' : 5,
                 'tau' : 5,
+                'prev_home': 0.5,
                 'prev_schools': 0.5,
                 'prev_work': 0.5,
                 'prev_rest': 0.5,
