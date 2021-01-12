ea170ed640f2c5c82bed1bcd2986145e4df6a06d
twallema <tijs.alleman@ugent.be>
2021-01-12 15:09:34 +0100

    add apple mobility data function

--- src/covid19model/data/mobility.py
+++ src/covid19model/data/mobility.py
@@ -6,6 +6,82 @@ import matplotlib.pyplot as plt
 
 from ..visualization.data import google_mobility
 
+def get_apple_mobility_data(update=True, plot=False, filename_plot=None):
+    """Download Apple Mobility Trends
+
+    This function downloads, formats and returns the available Apple Mobility Trends for the Provinces of Belgium.
+    A copy of the downloaded dataset is automatically saved in the /data/raw folder.
+
+    Parameters
+    -----------
+    update : boolean (default True)
+        True if you want to update the data,
+        False if you want to read only previously saved data
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        DataFrame containing the Apple mobility data on daily basis, starting February 15th 2020.
+        The dataframe has two indices: the spatial unit (f.e. 'Belgium' or 'Namur Province') and the mobility type ('walking', 'transit', 'driving').
+        Example indexing: df_apple.loc[('Belgium','transit')]
+
+    Notes
+    -----
+    Transport types (walking, transit, driving) are not available for every province.    
+    Data before February 15th 2020 was omitted to match the start date of the Google community reports.
+    Baseline was defined by Apple as the mobility on January 13th 2020. Data was rescaled using the mean mobility from February 15th 2020 until March 10th, 2020.
+    Project homepage: https://covid19.apple.com/mobility
+    Data extracted from: https://covid19-static.cdn-apple.com/covid19-mobility-data/2024HotfixDev12/v3/en-us/applemobilitytrends-2021-01-10.csv
+
+    Example use
+    -----------
+    df_apple = mobility.get_apple_mobility_data(update=False)
+    # Extracting transit mobility data for Belgium
+    df_apple.loc[('Belgium','transit')]
+    # Visualizing mobility data
+    plt.plot(df_apple.loc[('Belgium','transit')])
+    """
+
+    # Data source
+    url = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2024HotfixDev12/v3/en-us/applemobilitytrends-2021-01-10.csv'
+    abs_dir = os.path.dirname(__file__)
+
+    if update:
+        # download raw data
+        df_raw = pd.read_csv(url)
+        # save a copy in the raw folder
+        rel_dir = os.path.join(abs_dir, '../../../data/raw/mobility/apple/apple_mobility_trends.csv')
+        df_raw.to_csv(rel_dir, index=False)
+    else:
+        df_raw = pd.read_csv(os.path.join(abs_dir,
+            '../../../data/raw/mobility/apple/apple_mobility_trends.csv'))
+
+    # Start by extracting the overall data for Belgium
+    df = df_raw[df_raw['region']=='Belgium']
+    columns = pd.to_datetime(df_raw.columns[6:].values)
+    data = df.values[:,6:]
+    arrays = [
+        np.array(["Belgium","Belgium","Belgium"]),
+        np.array(["driving", "transit", "walking"]),
+    ]
+    df_apple = pd.DataFrame(data,index=arrays,columns=columns)
+
+    # Loop over all available provinces and transit types
+    df = df_raw[((df_raw['country']=='Belgium')&(df_raw['geo_type']=='sub-region'))]
+    for province in set(df['region']):
+        for mobility_type in df['transportation_type'][df['region'] == province]:
+            data = df[((df['transportation_type']==mobility_type)&(df['region']==province))].values[:,6:]
+            df_entry = pd.DataFrame(data,index=[[province],[mobility_type]],columns=columns)
+            df_apple=df_apple.append(df_entry)
+
+    # Re-define baseline and cut away data before February 15th 2020:
+    for spatial_unit,mobility_type in df_apple.index:
+        base=np.mean(df_apple.loc[(spatial_unit,mobility_type)][pd.to_datetime('2020-02-15'):pd.to_datetime('2020-03-10')])
+        df_apple.loc[(spatial_unit,mobility_type)] = -(100-df_apple.loc[(spatial_unit,mobility_type)]/base*100)
+        df_apple.loc[(spatial_unit,mobility_type)] = df_apple.loc[(spatial_unit,mobility_type)][pd.to_datetime('2020-02-15'):]
+
+    return df_apple
+
 def get_google_mobility_data(update=True, plot=False, filename_plot=None):
     """Download Google Community mobility report data
 
