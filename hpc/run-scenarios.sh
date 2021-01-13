9152c017dc4f780a848adda918c8fde0fe0a7bbe
vsc41096 vscuser <vsc41096@gligar04.gastly.os>
2021-01-13 08:30:58 +0100

    add run-scenarios.sh

---
+++ hpc/run-scenarios.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -N run-scenarios ## job name
+#PBS -l nodes=1:ppn=5 ## single-node job, on 9 cores
+#PBS -l walltime=72:00:00 ## max. 72h of wall time
+
+# Change to package folder
+cd /data/gent/vo/000/gvo00048/vsc41096/COVID19-Model/hpc/
+
+# Make script executable
+chmod +x jvergeyn-restore6.1.py
+
+# Activate conda environment
+source activate COVID_MODEL
+
+# Execute script
+python jvergeyn-restore6.1.py 1 2a 2b 2c 3 #(scenario-nummers)
+
+# Deactivate environment
+conda deactivate
