041a175c31b08636b3cc6b6635f92920757428c3
twallema <tijs.alleman@ugent.be>
2021-01-18 13:22:57 +0100

    added calibration of beta and warmup

--- notebooks/calibration/calibrate-COVID-19-SEIRD-WAVE1.py
+++ notebooks/calibration/calibrate-COVID-19-SEIRD-WAVE1.py
@@ -13,10 +13,13 @@ __copyright__   = "Copyright (c) 2020 by T.W. Alleman, BIOMATH, Ghent University
 
 import random
 import emcee
+import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 from covid19model.models import models
+from covid19model.optimization.run_optimization import checkplots
 from covid19model.data import mobility, sciensano, model_parameters
 from covid19model.optimization import pso, objective_fcns
 from covid19model.models.time_dependant_parameter_fncs import ramp_fun
@@ -28,6 +31,7 @@ from covid19model.visualization.output import _apply_tick_locator
 
 # Contact matrices
 initN, Nc_home, Nc_work, Nc_schools, Nc_transport, Nc_leisure, Nc_others, Nc_total = model_parameters.get_interaction_matrices(dataset='willem_2012')
+Nc_all = {'total': Nc_total, 'home':Nc_home, 'work': Nc_work, 'schools': Nc_schools, 'transport': Nc_transport, 'leisure': Nc_leisure, 'others': Nc_others}
 levels = initN.size
 # Sciensano data
 df_sciensano = sciensano.get_sciensano_COVID19_data(update=False)
@@ -38,18 +42,49 @@ df_google = mobility.get_google_mobility_data(update=False)
 # Time-dependant parameter function
 # ---------------------------------
 
-def compliance_func(t, param, l, tau, prevention):
+# Extract build contact matrix function
+from covid19model.models.time_dependant_parameter_fncs import make_contact_matrix_function, ramp_fun
+contact_matrix_4prev = make_contact_matrix_function(df_google, Nc_all)
+
+# Define policy function
+def policies_wave1_4prev(t, param, l , tau, 
+                   prev_schools, prev_work, prev_rest, prev_home):
+    
     # Convert tau and l to dates
     tau_days = pd.Timedelta(tau, unit='D')
     l_days = pd.Timedelta(l, unit='D')
-    # Measures
-    start_measures = pd.to_datetime('2020-03-15')
-    if t < start_measures + tau_days:
-        return param
-    elif start_measures + tau_days < t <= start_measures + tau_days + l_days:
-        return ramp_fun(param, prevention*param, t, tau_days, l, start_measures)
+
+    # Define additional dates where intensity or school policy changes
+    t1 = pd.Timestamp('2020-03-15') # start of lockdown
+    t2 = pd.Timestamp('2020-05-15') # gradual re-opening of schools (assume 50% of nominal scenario)
+    t3 = pd.Timestamp('2020-07-01') # start of summer holidays
+    t4 = pd.Timestamp('2020-09-01') # end of summer holidays
+
+    if t1 + tau_days <= t:
+        t = pd.Timestamp(t.date())
+        return contact_matrix_4prev(t, school=1)
+    elif t1 + tau_days < t <= t1 + tau_days + l_days:
+        t = pd.Timestamp(t.date())
+        policy_old = contact_matrix_4prev(t, school=1)
+        policy_new = contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
+                                    school=0)
+        return ramp_fun(policy_old, policy_new, t, tau_days, l, t1)
+    elif t1 + tau_days + l_days < t <= t2:
+        t = pd.Timestamp(t.date())
+        contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
+                              school=0)
+    elif t2 < t <= t3:
+        t = pd.Timestamp(t.date())
+        contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
+                              school=0.5)
+    elif t3 < t <= t4:
+        t = pd.Timestamp(t.date())
+        contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
+                              school=0)                     
     else:
-        return param * prevention
+        t = pd.Timestamp(t.date())
+        return contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
+                              school=1)
 
 # --------------------
 # Initialize the model
@@ -58,62 +93,77 @@ def compliance_func(t, param, l, tau, prevention):
 # Load the model parameters dictionary
 params = model_parameters.get_COVID19_SEIRD_parameters()
 # Add the time-dependant parameter function arguments
-params.update({'l': 1, 'tau': 1, 'prevention' : 0.5})
+params.update({'l': 1, 'tau': 14, 'prev_schools': 0.5, 'prev_work': 0.5, 'prev_rest': 0.5, 'prev_home': 0.5})
 # Define initial states
-initial_states = {"S": initN, "E": 3*np.ones(9)}
+initial_states = {"S": initN, "E": np.ones(9)}
 # Initialize model
 model = models.COVID19_SEIRD(initial_states, params,
-                        time_dependent_parameters={'Nc': compliance_func})
+                        time_dependent_parameters={'Nc': policies_wave1_4prev})
 
-# ---------------------------
-# Define calibration settings
-# ---------------------------
+# ------------------------
+# Define results locations
+# ------------------------
+
+# Path where samples bakcend should be stored
+results_folder = "../../results/calibrations/COVID19_SEIRD/national/backends/"
+# Path where figures should be stored
+fig_path = '../../results/calibrations/COVID19_SEIRD/national/'
+# Path where MCMC samples should be saved
+samples_path = '../../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/'
+
+# ------------------------------------------
+# Perform the calibration of warmup and beta
+# ------------------------------------------
 
 # Spatial unit: Belgium
-spatial_unit = 'BE_dummy'
+spatial_unit = 'BE_WAVE1'
 # Start of data collection
 start_data = '2020-03-15'
 # Start data of recalibration ramp
 start_calibration = '2020-03-15'
-# Last datapoint used to recalibrate the ramp
+# Last datapoint used to calibrate warmup and beta
+end_calibration_beta = '2020-05-01'
+# Last datapoint used to calibrate the ramp and prevention parameters
 end_calibration = '2020-05-01'
 # PSO settings
-warmup=0
-maxiter = 5
-popsize = 2
+processes = 5 
+multiplier = 5
+maxiter = 50
+popsize = multiplier*processes
 # MCMC settings
-steps_mcmc = 5
+steps_mcmc = 50
 discard = 0
 # define dataset
-data=[df_sciensano['H_in'][start_calibration:end_calibration]]
+data=[df_sciensano['H_in'][start_calibration:end_calibration_beta]]
 states = [["H_in"]]
 
-# ---------------------------------------
-# Perform a calibration on the first wave
-# ---------------------------------------
 
-print('------------------------------')
-print('PERFORMING CALIBRATION')
-print('------------------------------\n')
-print('Using data from '+start_calibration+' until '+end_calibration+'\n')
+print('-----------------------------------------')
+print('PERFORMING CALIBRATION OF BETA AND WARMUP')
+print('-----------------------------------------\n')
+print('Using data from '+start_calibration+' until '+end_calibration_beta+'\n')
 print('1) Particle swarm optimization\n')
+print('Using ' + str(processes) + ' cores\n')
 
 # set PSO optimisation settings
-parNames = ['warmup','beta','l','tau','prevention']
-bounds=((20,80),(0.01,0.06),(0.1,20),(0.1,20),(0.03,0.97))
+parNames = ['warmup','beta']
+bounds=((20,80),(0.01,0.06))
 
 # run PSO optimisation
 theta = pso.fit_pso(model,data,parNames,states,bounds,maxiter=maxiter,popsize=popsize,
-                    start_date=start_calibration)
+                    start_date=start_calibration, processes=processes)
 warmup = int(theta[0])
-theta = np.array([0.02390738,0.93245834,11.76934931,0.03]) #instead of: theta[1:]
 
 # run MCMC sampler
 print('\n2) Markov-Chain Monte-Carlo sampling\n')
 
+# Set up the sampler backend
+filename = spatial_unit+'_'+str(datetime.date.today())
+backend = emcee.backends.HDFBackend(results_folder+filename)
+
 # Setup parameter names, bounds, number of chains, etc.
-parNames_mcmc = ['beta','l','tau','prevention']
-bounds_mcmc=((0.010,0.060),(0.001,20),(0.001,20),(0,1))
+parNames_mcmc = ['beta']
+bounds_mcmc=((0.010,0.060))
 ndim = len(theta)
 nwalkers = ndim*2
 perturbations = theta*1e-2*np.random.random(size=(nwalkers,ndim))
@@ -128,9 +178,11 @@ for i in range(pos.shape[0]):
             pos[i,j] = bounds_mcmc[j][1]
 
 # Run sampler
-sampler = emcee.EnsembleSampler(nwalkers, ndim, objective_fcns.log_probability,
-            args=(model, bounds_mcmc, data, states, parNames_mcmc, None, start_calibration, warmup,'poisson'))
-sampler.run_mcmc(pos, steps_mcmc, progress=True)
+from multiprocessing import Pool
+with Pool() as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, objective_fcns.log_probability,backend=backend,pool=pool,
+                args=(model, bounds_mcmc, data, states, parNames_mcmc, None, start_calibration, warmup,'poisson'))
+    sampler.run_mcmc(pos, steps_mcmc, progress=True)
 
 thin = 1
 try:
@@ -139,12 +191,16 @@ try:
 except:
     print('Warning: The chain is shorter than 50 times the integrated autocorrelation time.\nUse this estimate with caution and run a longer chain!\n')
 
+checkplots(sampler, discard, thin, fig_path, spatial_unit, figname='FIT_WAVE1_BETA', 
+           labels=['$\\beta$'])
+
 print('\n3) Sending samples to dictionary')
 
 flat_samples = sampler.get_chain(discard=discard,thin=thin,flat=True)
 samples_dict = {}
 for count,name in enumerate(parNames_mcmc):
     samples_dict[name] = flat_samples[:,count].tolist()
+samples_dict['warmup'] = warmup
 
 # ------------------------
 # Define sampling function
@@ -153,9 +209,6 @@ for count,name in enumerate(parNames_mcmc):
 def draw_fcn(param_dict,samples_dict,to_sample):
     # Sample
     idx, param_dict['beta'] = random.choice(list(enumerate(samples_dict['beta'])))
-    param_dict['l'] = samples_dict['l'][idx] 
-    param_dict['tau'] = samples_dict['tau'][idx]      
-    param_dict['prevention'] = samples_dict['prevention'][idx]
     return param_dict
 
 # ----------------------
@@ -164,8 +217,8 @@ def draw_fcn(param_dict,samples_dict,to_sample):
 
 print('\n4) Simulating using sampled parameters')
 start_sim = start_calibration
-end_sim = end_calibration
-n_samples = 4
+end_sim = '2020-03-23'
+n_samples = 5
 out = model.sim(end_sim,start_date=start_sim,warmup=warmup,N=n_samples,draw_fcn=draw_fcn,samples=samples_dict)
 
 # ---------------------------
@@ -205,4 +258,5 @@ ax.fill_between(pd.to_datetime(out['time'].values),H_in_LL, H_in_UL,alpha=0.20, 
 ax.plot(out['time'],H_in_mean,'--', color='blue')
 ax.scatter(df_sciensano[start_sim:end_sim].index,df_sciensano['H_in'][start_sim:end_sim],color='black',alpha=0.4,linestyle='None',facecolors='none')
 ax = _apply_tick_locator(ax)
-#plt.show()
+ax.set_xlim('2020-03-10',end_sim)
+fig.savefig(fig_path+'others/FIT_BETA_'+spatial_unit+'_'+str(datetime.date.today())+'.pdf', dpi=400, bbox_inches='tight')
