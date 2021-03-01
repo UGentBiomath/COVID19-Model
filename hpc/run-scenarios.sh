8204cbfbce7018896d20ac94732409eeac75ec04
vsc41096 vscuser <vsc41096@gligar05.gastly.os>
2021-03-01 08:05:41 +0100

    add vaccination scenarios

--- hpc/run-scenarios.sh
+++ hpc/run-scenarios.sh
@@ -7,14 +7,16 @@
 cd /data/gent/vo/000/gvo00048/vsc41096/COVID19-Model/hpc/
 
 # Make script executable
-chmod +x jvergeyn-restore7.py
+#chmod +x jvergeyn-restore7.py
+chmod +x jvergeyn-vacc-strategies.py
 
 # Activate conda environment
 source activate COVID_MODEL
 
 # Execute script
 #python jvergeyn-restore6.1.py 1 2a 2b 2c #(scenario-nummers)
-python jvergeyn-restore7.py
+#python jvergeyn-restore7.py
+python jvergeyn-vacc-strategies.py
 
 # Deactivate environment
 conda deactivate
