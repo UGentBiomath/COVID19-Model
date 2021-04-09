19acc809f52f6f20fe077db96548151c15983bc6
Michiel Rollier <Michiel.Rollier@UGent.be>
2021-04-09 11:36:31 +0200

    Prepare bash file for job execution on HPC

--- hpc/run-calibration-WAVE1_arr.sh
+++ hpc/run-calibration-WAVE1_arr.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=36 ## single-node job, on 36 cores
 #PBS -l walltime=72:00:00 ## max. 72h of wall time. This is OK because the node has been reserved anyway
 
-
+#####################################################
 # Choose initial condition
 init=BXL
 
@@ -11,21 +11,25 @@ init=BXL
 maxiter=50
 number=100
 
+# Choose name of output files
+signature=${init}_${maxiter}xPSO_${number}xMCMC_arr
+echo Initiating job with signature \"${signature}\"
+#####################################################
 
 # Switch to the reserved cluster
 module swap cluster/skitty
 
-# Change to package folder
+# Change to package folder (probably superfluous)
 cd $VSC_HOME/Documents/COVID19-Model/hpc/
 
 # Make script executable
-chmod +x calibrate-COVID-19-SEIRD-WAVE1.py
+# chmod +x calibrate-COVID-19-SEIRD-WAVE1.py
 
 # Activate conda environment
 source activate COVID_MODEL
 
 # Execute script with correct parsed arguments
-python mrollier-calibration-WAVE1_arr.py -i ${init} -m ${maxiter} -n ${number}
+python mrollier-calibration-WAVE1_arr.py -i ${init} -m ${maxiter} -n ${number} -s ${signature}
 
 # Deactivate environment
 conda deactivate
