2c752e25e5d8c6869a8ac1a5ffe50146b117ee95
twallema <tijs.alleman@ugent.be>
2021-04-26 12:00:49 +0200

    working calibration script WAVE 2

--- notebooks/calibration/calibrate-COVID-19-SEIRD-WAVE2.py
+++ notebooks/calibration/calibrate-COVID-19-SEIRD-WAVE2.py
@@ -1,5 +1,5 @@
 """
-This script contains a four-prevention parameter, two-parameter delayed compliance ramp calibration to hospitalization data from the first COVID-19 wave in Belgium.
+This script contains a four-prevention parameter, two-parameter delayed compliance ramp calibration to hospitalization data from the second COVID-19 wave in Belgium.
 Deterministic, national-level BIOMATH COVID-19 SEIRD
 Its intended use is the calibration for the descriptive manuscript: "A deterministic, age-stratified, extended SEIRD model for investigating the effect of non-pharmaceutical interventions on SARS-CoV-2 spread in Belgium".
 """
@@ -84,6 +84,9 @@ run_date = str(datetime.date.today())
 initN, Nc_home, Nc_work, Nc_schools, Nc_transport, Nc_leisure, Nc_others, Nc_total = model_parameters.get_interaction_matrices(dataset='willem_2012')
 Nc_all = {'total': Nc_total, 'home':Nc_home, 'work': Nc_work, 'schools': Nc_schools, 'transport': Nc_transport, 'leisure': Nc_leisure, 'others': Nc_others}
 levels = initN.size
+# Use time-integrated matrices
+intmat = model_parameters.get_integrated_interaction_matrices()
+Nc_all = {'total': intmat['Nc_total'], 'home': intmat['Nc_home'], 'work': intmat['Nc_work'], 'schools': intmat['Nc_schools'], 'transport': intmat['Nc_transport'], 'leisure': intmat['Nc_leisure'], 'others': intmat['Nc_others']}
 # Sciensano data
 df_sciensano = sciensano.get_sciensano_COVID19_data(update=False)
 # Google Mobility data
@@ -92,14 +95,28 @@ df_google = mobility.get_google_mobility_data(update=False)
 warmup = 0
 with open('../../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/initial_states_2020-09-01.json', 'r') as fp:
     initial_states = json.load(fp)    
-
-initial_states.update({
-    'VE': np.zeros(9),
-    'V': np.zeros(9),
-    'V_new': np.zeros(9),
-    'alpha': np.zeros(9)
-})
-#initial_states['ICU_tot'] = initial_states.pop('ICU')
+# Add additional states of vaccination model
+initial_states.update({'S_v': np.zeros(9), 'E_v': np.zeros(9), 'I_v': np.zeros(9),
+                        'A_v': np.zeros(9), 'M_v': np.zeros(9), 'ER_v': np.zeros(9), 'C_v': np.zeros(9),
+                        'C_icurec_v': np.zeros(9), 'ICU_v': np.zeros(9), 'R_v': np.zeros(9)})
+# Load and format serodata of Herzog
+df_sero = pd.read_csv('../../data/interim/sero/sero_national_overall_herzog.csv', parse_dates=True)
+df_sero.index = df_sero['collection_midpoint']
+df_sero.index = pd.to_datetime(df_sero.index)
+df_sero = df_sero.drop(columns=['collection_midpoint','age_cat'])
+df_sero['mean'] = df_sero['mean']*sum(initN) 
+df_sero_herzog = df_sero
+# Load and format serodata of Sciensano
+df_sero = pd.read_csv('../../data/raw/sero/Belgium COVID-19 Studies - Sciensano_Blood Donors_Tijdreeks.csv', parse_dates=True)
+df_sero.index = df_sero['Date']
+df_sero.index = pd.to_datetime(df_sero.index)
+df_sero = df_sero.drop(columns=['Date'])
+df_sero['mean'] = df_sero['mean']*sum(initN) 
+df_sero_sciensano = df_sero
+# Samples of resusceptibility
+with open('../../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/BE_WAVE1_R0_COMP_EFF_2021-04-23.json', 'r') as fp:
+    samples_dict = json.load(fp)
+samples_dict = {'zeta': samples_dict['zeta']}
 
 # ------------------------
 # Define results locations
@@ -112,19 +129,99 @@ fig_path = '../../results/calibrations/COVID19_SEIRD/national/'
 # Path where MCMC samples should be saved
 samples_path = '../../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/'
 
-# ---------------------------------
-# Time-dependant parameter function
-# ---------------------------------
+# ---------------------------
+# Time-dependant VOC function
+# ---------------------------
+
+from covid19model.models.time_dependant_parameter_fncs import make_VOC_function
+VOC_function = make_VOC_function()
+
+def VOC_wrapper_func(t,states,param):
+    t = pd.Timestamp(t.date())
+    return VOC_function(t)
+
+# -----------------------------------
+# Time-dependant vaccination function
+# -----------------------------------
+
+from covid19model.models.time_dependant_parameter_fncs import  make_vaccination_function
+sciensano_first_dose, df_sciensano_start, df_sciensano_end = make_vaccination_function(df_sciensano)
+
+def vacc_strategy(t, states, param, df_sciensano_start, df_sciensano_end,
+                    daily_dose=30000, delay = 21, vacc_order = [8,7,6,5,4,3,2,1,0], refusal = [0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3]):
+    """
+    time-dependent function for the Belgian vaccination strategy
+    First, all available data from Sciensano are used. Then, the user can specify a custom vaccination strategy of "daily_dose" doses per day,
+    given in the order specified by the vector "vacc_order" with a refusal propensity of "refusal" in every age group.
+  
+    Parameters
+    ----------
+    t : int
+        Simulation time
+    states: dict
+        Dictionary containing values of model states
+    param : dict
+        Model parameter dictionary
+    sciensano_first_dose : function
+        Function returning the number of (first dose) vaccinated individuals at simulation time t, according to the data made public by Sciensano.
+    df_sciensano_start : date
+        Start date of Sciensano vaccination data frame
+    df_sciensano_end : date
+        End date of Sciensano vaccination data frame
+    daily_dose : int
+        Number of doses administered per day. Default is 30000 doses/day.
+    delay : int
+        Time delay between first dose vaccination and start of immunity. Default is 21 days.
+    vacc_order : array
+        Vector containing vaccination prioritization preference. Default is old to young. Must be equal in length to the number of age bins in the model.
+    refusal: array
+        Vector containing the fraction of individuals refusing a vaccine per age group. Default is 30% in every age group. Must be equal in length to the number of age bins in the model.
+
+    Return
+    ------
+    N_vacc : array
+        Number of individuals to be vaccinated at simulation time "t"
+        
+    """
+
+    # Convert time to suitable format
+    t = pd.Timestamp(t.date())
+    # Convert delay to a timedelta
+    delay = pd.Timedelta(str(int(delay))+'D')
+    # Compute the number of vaccine eligible individuals
+    VE = states['S'] + states['R']
+    
+    if t <= df_sciensano_start + delay:
+        return np.zeros(9)
+    elif df_sciensano_start + delay < t <= df_sciensano_end + delay:
+        return sciensano_first_dose(t-delay)
+    else:
+        N_vacc = np.zeros(9)
+        # Vaccines distributed according to vector 'order'
+        # With residue 'refusal' remaining in each age group
+        idx = 0
+        while daily_dose > 0:
+            if VE[vacc_order[idx]]*(1-refusal[vacc_order[idx]]) > daily_dose:
+                N_vacc[vacc_order[idx]] = daily_dose
+                daily_dose = 0
+            else:
+                N_vacc[vacc_order[idx]] = VE[vacc_order[idx]]*(1-refusal[vacc_order[idx]])
+                daily_dose = daily_dose - VE[vacc_order[idx]]*(1-refusal[vacc_order[idx]])
+                idx = idx + 1
+        return N_vacc
+
+# --------------------------------------
+# Time-dependant social contact function
+# --------------------------------------
 
 # Extract build contact matrix function
 from covid19model.models.time_dependant_parameter_fncs import make_contact_matrix_function, ramp_fun
 contact_matrix_4prev, all_contact, all_contact_no_schools = make_contact_matrix_function(df_google, Nc_all)
 
 # Define policy function
-def policies_wave1_4prev(t, states, param, l , tau, prev_schools, prev_work, prev_rest, prev_home):
+def policies_wave2(t, states, param, l , prev_schools, prev_work, prev_rest, prev_home):
     
     # Convert tau and l to dates
-    tau_days = pd.Timedelta(tau, unit='D')
     l_days = pd.Timedelta(l, unit='D')
 
     # Define key dates of first wave
@@ -141,21 +238,21 @@ def policies_wave1_4prev(t, states, param, l , tau, prev_schools, prev_work, pre
     t9 = pd.Timestamp('2021-01-04') # Christmas holiday ends
     t10 = pd.Timestamp('2021-02-15') # Spring break starts
     t11 = pd.Timestamp('2021-02-21') # Spring break ends
-    t12 = pd.Timestamp('2021-04-05') # Easter holiday starts
+    t12 = pd.Timestamp('2021-03-26') # Easter holiday starts
     t13 = pd.Timestamp('2021-04-18') # Easter holiday ends
 
     t = pd.Timestamp(t.date())
     # First wave
     if t <= t1:
         return all_contact(t)
-    elif t1 < t < t1 + tau_days:
+    elif t1 < t < t1:
         return all_contact(t)
-    elif t1 + tau_days < t <= t1 + tau_days + l_days:
+    elif t1  < t <= t1  + l_days:
         policy_old = all_contact(t)
         policy_new = contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
                                     school=0)
-        return ramp_fun(policy_old, policy_new, t, tau_days, l, t1)
-    elif t1 + tau_days + l_days < t <= t2:
+        return ramp_fun(policy_old, policy_new, t, t1, l)
+    elif t1 + l_days < t <= t2:
         return contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
                               school=0)
     elif t2 < t <= t3:
@@ -164,14 +261,14 @@ def policies_wave1_4prev(t, states, param, l , tau, prev_schools, prev_work, pre
     elif t3 < t <= t4:
         return contact_matrix_4prev(t, school=0)
     # Second wave
-    elif t4 < t <= t5 + tau_days:
+    elif t4 < t <= t5:
         return contact_matrix_4prev(t, school=1)
-    elif t5 + tau_days < t <= t5 + tau_days + l_days:
+    elif t5 < t <= t5 + l_days:
         policy_old = contact_matrix_4prev(t, school=1)
         policy_new = contact_matrix_4prev(t, prev_schools, prev_work, prev_rest, 
                                     school=1)
-        return ramp_fun(policy_old, policy_new, t, tau_days, l, t5)
-    elif t5 + tau_days + l_days < t <= t6:
+        return ramp_fun(policy_old, policy_new, t, t5, l)
+    elif t5 + l_days < t <= t6:
         return contact_matrix_4prev(t, prev_home, prev_schools, prev_work, prev_rest, 
                               school=1)
     elif t6 < t <= t7:
@@ -220,6 +317,21 @@ def add_poisson(state_name, output, n_samples, n_draws_per_sample, UL=1-0.05*0.5
     UL = np.quantile(vector, q = UL, axis = 1)
     return mean, median, LL, UL
 
+# ------------------------
+# Define sampling function
+# ------------------------
+
+def draw_fcn(param_dict,samples_dict):
+    # Re-susceptibility as estimated from first 2020 COVID-19 wave
+    idx, param_dict['zeta'] = random.choice(list(enumerate(samples_dict['zeta'])))
+    # Sensitivity analysis on vaccination parameters
+    param_dict['e_i'] = np.random.uniform(low=0.8,high=1) # Vaccinated individual is 80-100% less infectious than non-vaccinated indidivudal
+    param_dict['e_s'] = np.random.uniform(low=0.85,high=0.95) # Vaccine results in a 85-95% lower susceptibility
+    param_dict['e_h'] = np.random.uniform(low=0.5,high=1.0) # Vaccine blocks hospital admission between 50-100%
+    # Bootstrap the delay between vaccination and protection
+    param_dict['delay'] = np.mean(np.random.triangular(1, 45, 45, size=50))
+    return param_dict
+
 #############
 ## JOB: R0 ##
 #############
@@ -240,7 +352,7 @@ else:
 spatial_unit = 'BE_WAVE2'
 # PSO settings
 processes = mp.cpu_count()
-multiplier = 5
+multiplier = 3
 maxiter = 20
 popsize = multiplier*processes
 # MCMC settings
@@ -255,12 +367,25 @@ n_draws_per_sample=1
 # --------------------
 
 # Load the model parameters dictionary
-params = model_parameters.get_COVID19_SEIRD_parameters()
+params = model_parameters.get_COVID19_SEIRD_parameters(vaccination=True)
 # Add the time-dependant parameter function arguments
-params.update({'l': 21, 'tau': 21, 'prev_schools': 0, 'prev_work': 0.5, 'prev_rest': 0.5, 'prev_home': 0.5})
+# Social policies
+params.update({'l': 21, 'prev_schools': 0, 'prev_work': 0.5, 'prev_rest': 0.5, 'prev_home': 0.5})
+# Vaccination
+vacc_order = np.array(range(9))
+vacc_order = vacc_order[::-1]
+daily_dose = 55000
+refusal = 0.2*np.ones(9)
+delay = 21
+d_vacc = 12*30 # duration of vaccine protection
+params.update(
+    {'vacc_order': vacc_order, 'daily_dose': daily_dose,
+     'refusal': refusal, 'delay': delay, 'df_sciensano_start': df_sciensano_start,
+     'df_sciensano_end': df_sciensano_end}
+)
 # Initialize model
-model = models.COVID19_SEIRD(initial_states, params,
-                        time_dependent_parameters={'Nc': policies_wave1_4prev})
+model = models.COVID19_SEIRD_vacc(initial_states, params,
+                        time_dependent_parameters={'Nc': policies_wave2, 'N_vacc': vacc_strategy, 'alpha': VOC_wrapper_func})
 
 if job == 'R0':
 
@@ -486,7 +611,6 @@ if job == 'R0':
     # ---------------
     # Visualizing fit
     # ---------------
-
     print('6) Visualizing fit \n')
 
     # Plot
@@ -521,18 +645,18 @@ start_data = '2020-03-15'
 start_calibration = '2020-09-01'
 # Last datapoint used to calibrate compliance and prevention
 if not args.enddate:
-    end_calibration = '2021-02-01'
+    end_calibration = '2021-04-21'
 else:
     end_calibration = str(args.enddate)
 # PSO settings
 processes = mp.cpu_count()
-multiplier = 3
-maxiter = 30
+multiplier = 5
+maxiter = 100
 popsize = multiplier*processes
 # MCMC settings
-max_n = 50
+max_n = 500000
 # Number of samples used to visualise model fit
-n_samples = 20
+n_samples = 200
 # Number of binomial draws per sample drawn used to visualize model fit
 n_draws_per_sample=1
 
@@ -548,41 +672,36 @@ print('Using ' + str(processes) + ' cores\n')
 # --------------
 
 data=[df_sciensano['H_in'][start_calibration:end_calibration]]
-states = [["H_in"]]
-
-# ------------------------
-# Define sampling function
-# ------------------------
-
-def draw_fcn(param_dict,samples_dict):
-    param_dict['sigma'] = 5.2 - param_dict['omega']
-    return param_dict
+states = ["H_in"]
+weights = [1]
 
 # -----------
 # Perform PSO
 # -----------
 
 # optimisation settings
-parNames = ['beta','omega','da','l', 'tau', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home']
-bounds=((0.01,0.03),(0.1,1.0),(10,14),(3,7),(0.1,2),(0.70,0.99),(0.01,0.50),(0.01,0.50),(0.70,0.99))
+parNames = ['beta','da','l', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home', 'K_inf', 'K_hosp']
+bounds=((0.01,0.04),(4,14),(4.5,14),(0.40,0.99),(0.10,0.60),(0.10,0.60),(0.40,0.99),(1,1.6),(1,1.6))
 
 # run optimization
-theta = pso.fit_pso(model, data, parNames, states, bounds, maxiter=maxiter, popsize=popsize,
-                    start_date=start_calibration, warmup=warmup, processes=processes,
-                    draw_fcn=draw_fcn, samples={})
-# Calibration until 2021-02-01
-#theta = np.array([2.42597349e-02, 2.00000000e-01, 1.4000000e+01, 6.64616323e+00, 3.96027778e-01, 7.89238758e-01, 3.55888406e-01, 5.0e-02, 7.49952898e-01]) #-159676.96370239937
+#theta = pso.fit_pso(model, data, parNames, states, weights, bounds, maxiter=maxiter, popsize=popsize,
+#                    start_date=start_calibration, warmup=warmup, processes=processes,
+#                    draw_fcn=draw_fcn, samples=samples_dict)
+# Calibration until 2021-04-21
+theta = np.array([0.01426078, 9.75610512, 5.32524597, 0.84720035, 0.21093236, 0.10595917, 0.43611962, 1.48916965, 1.06736707]) #-222310.27309159632
+# The MCMC sampler converges to the estimate below --> directly start there
+theta = np.array([0.01375, 11, 4.5, 0.6, 0.1, 0.07, 0.90, 1.50, 1.30])
 
 # assign results
 model.parameters['beta'] = theta[0]
-model.parameters['omega'] = theta[1]
-model.parameters['da'] = theta[2]
-model.parameters['l'] = theta[3]
-model.parameters['tau'] = theta[4]
-model.parameters['prev_schools'] = theta[5]
-model.parameters['prev_work'] = theta[6]
-model.parameters['prev_rest'] = theta[7]
-model.parameters['prev_home'] =  theta[8]
+model.parameters['da'] = theta[1]
+model.parameters['l'] = theta[2]
+model.parameters['prev_schools'] = theta[3]
+model.parameters['prev_work'] = theta[4]
+model.parameters['prev_rest'] = theta[5]
+model.parameters['prev_home'] =  theta[6]
+model.parameters['K_inf'] = theta[7]
+model.parameters['K_hosp'] = theta[8]
 
 # -----------------
 # Visualise PSO fit
@@ -590,8 +709,8 @@ model.parameters['prev_home'] =  theta[8]
 
 # Simulate
 start_sim = start_calibration
-end_sim = '2021-04-01'
-out = model.sim(end_sim,start_date=start_sim,warmup=warmup,draw_fcn=draw_fcn,samples={})
+end_sim = '2021-07-01'
+out = model.sim(end_sim,start_date=start_sim,warmup=warmup,draw_fcn=draw_fcn,samples=samples_dict)
 # Plot
 fig,ax = plt.subplots(figsize=(10,5))
 ax.plot(out['time'],out['H_in'].sum(dim='Nc'),'--', color='blue')
@@ -622,24 +741,32 @@ print('\n2) Markov Chain Monte Carlo sampling\n')
 #density_da_norm = density_da/np.sum(density_da)
 
 # Setup uniform priors
-parNames_mcmc = ['beta','omega','da','l', 'tau', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home']
+parNames_mcmc = ['beta', 'da', 'l', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home','K_inf','K_hosp']
 log_prior_fnc = [prior_uniform, prior_uniform, prior_uniform, prior_uniform, prior_uniform, prior_uniform, prior_uniform, prior_uniform, prior_uniform]
-log_prior_fnc_args = [(0.005, 0.15),(0.1, 5.1),(13.5, 20),(0.1,20), (0.1,20), (0,1), (0,1), (0,1), (0,1)]
+log_prior_fnc_args = [(0.001, 0.12), (4, 14), (0.1,14), (0.05,1), (0.05,1), (0.05,1), (0.05,1),(1,1.8),(1,1.8)]
 ndim = len(parNames_mcmc)
-nwalkers = ndim*4
+nwalkers = ndim*2
 
 # Perturbate PSO Estimate
 pos = np.zeros([nwalkers,ndim])
 # Beta
-pos[:,0] = theta[0] + theta[0]*1e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
-# Omega and da
-pos[:,1:3] = theta[1:3] + theta[1:3]*1e-2*np.random.uniform(low=-1,high=1,size=(nwalkers,2))
-# l and tau
-pos[:,3:5] = theta[3:5] + theta[3:5]*1e-2*np.random.uniform(low=-1,high=1,size=(nwalkers,2))
+pos[:,0] = theta[0] + theta[0]*5e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# da
+pos[:,1] = theta[1] + theta[1]*5e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# l
+pos[:,2] = theta[2] + theta[2]*10e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
 # prevention schools
-pos[:,5] = theta[5] + theta[5]*1e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
-# other prevention
-pos[:,6:] = theta[6:] + theta[6:]*1e-2*np.random.uniform(low=-1,high=1,size=(nwalkers,len(theta[6:])))
+pos[:,3] = theta[3] + theta[3]*10e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# prevention work
+pos[:,4] = theta[4] + theta[4]*20e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# prevention rest
+pos[:,5] = theta[5] + theta[5]*20e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# prevention home
+pos[:,6] = theta[6] + theta[6]*20e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# K_inf
+pos[:,7] = theta[7] + theta[7]*10e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
+# K_hosp
+pos[:,8] = theta[8] + theta[8]*10e-2*np.random.uniform(low=-1,high=1,size=(nwalkers))
 
 # Set up the sampler backend if needed
 if backend:
@@ -659,11 +786,11 @@ old_tau = np.inf
 # Initialize autocorr vector and autocorrelation figure
 autocorr = np.zeros([1,ndim])
 # Initialize the labels
-labels = ['beta','omega','da','l', 'tau', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home']
+labels = ['beta','da','l', 'prev_schools', 'prev_work', 'prev_rest', 'prev_home', 'K_inf', 'K_hosp']
 
 with Pool() as pool:
     sampler = emcee.EnsembleSampler(nwalkers, ndim, objective_fcns.log_probability,backend=backend,pool=pool,
-                    args=(model,log_prior_fnc, log_prior_fnc_args, data, states, parNames_mcmc, draw_fcn, {}, start_calibration, warmup,'poisson'))
+                    args=(model,log_prior_fnc, log_prior_fnc_args, data, states, weights, parNames_mcmc, draw_fcn, samples_dict, start_calibration, warmup,'poisson'))
     for sample in sampler.sample(pos, iterations=max_n, progress=True, store=True):
        
         if sampler.iteration % 100:
