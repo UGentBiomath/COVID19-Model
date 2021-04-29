550e4d87c5e19798fc2fd2a80311238a801bedea
twallema <tijs.alleman@ugent.be>
2021-04-29 15:14:31 +0200

    add hospitalization + deaths fit

--- notebooks/calibration/plot_fit_R0_COMP_EFF_WAVE1.py
+++ notebooks/calibration/plot_fit_R0_COMP_EFF_WAVE1.py
@@ -49,9 +49,17 @@ args = parser.parse_args()
 # Load samples dictionary
 # -----------------------
 
+# Load calibration of WAVE 1
 samples_dict = json.load(open('../../data/interim/model_parameters/COVID19_SEIRD/calibrations/national/'+str(args.filename)))
-
 warmup = int(samples_dict['warmup'])
+# Append data on hospitalizations
+residence_time_distributions = pd.read_excel('../../data/interim/model_parameters/COVID19_SEIRD/sciensano_hospital_parameters.xlsx', sheet_name='residence_times', index_col=0, header=[0,1])
+samples_dict.update({'residence_times': residence_time_distributions})
+bootstrap_fractions = np.load('../../data/interim/model_parameters/COVID19_SEIRD/sciensano_bootstrap_fractions.npy')
+# First axis: parameter: c, m0, m0_C, m0_ICU
+# Second axis: age group
+# Third axis: bootstrap sample
+samples_dict.update({'samples_fractions': bootstrap_fractions})
 
 # Start of data collection
 start_data = '2020-03-15'
@@ -70,28 +78,23 @@ conf_int = 0.05
 initN, Nc_home, Nc_work, Nc_schools, Nc_transport, Nc_leisure, Nc_others, Nc_total = model_parameters.get_interaction_matrices(dataset='willem_2012')
 Nc_all = {'total': Nc_total, 'home':Nc_home, 'work': Nc_work, 'schools': Nc_schools, 'transport': Nc_transport, 'leisure': Nc_leisure, 'others': Nc_others}
 levels = initN.size
-
 intmat = model_parameters.get_integrated_interaction_matrices()
 Nc_all = {'total': intmat['Nc_total'], 'home': intmat['Nc_home'], 'work': intmat['Nc_work'], 'schools': intmat['Nc_schools'], 'transport': intmat['Nc_transport'], 'leisure': intmat['Nc_leisure'], 'others': intmat['Nc_others']}
-
-# Sciensano data
+# Sciensano public data
 df_sciensano = sciensano.get_sciensano_COVID19_data(update=False)
+# Sciensano mortality data
+df_sciensano_mortality = pd.read_csv('../../data/interim/sciensano/sciensano_detailed_mortality.csv', index_col=[0,1])
+# Convert to hospital deaths
+for idx,age_group in enumerate(df_sciensano_mortality.index.get_level_values(0).unique().values):
+    if idx == 0:
+        total_deaths_hospital = df_sciensano_mortality.xs(key=age_group, level="age_class", drop_level=True)[['cumsum_hospital']].values
+    else:
+        total_deaths_hospital = total_deaths_hospital + df_sciensano_mortality.xs(key=age_group, level="age_class", drop_level=True)[['cumsum_hospital']].values
+deaths_hospital = pd.Series(data=np.squeeze(total_deaths_hospital), index=df_sciensano_mortality.index.get_level_values(1).unique())
 # Google Mobility data
 df_google = mobility.get_google_mobility_data(update=False)
-# Load and format serodata of Herzog
-df_sero = pd.read_csv('../../data/interim/sero/sero_national_overall_herzog.csv', parse_dates=True)
-df_sero.index = df_sero['collection_midpoint']
-df_sero.index = pd.to_datetime(df_sero.index)
-df_sero = df_sero.drop(columns=['collection_midpoint','age_cat'])
-df_sero['mean'] = df_sero['mean']*sum(initN) 
-df_sero_herzog = df_sero
-# Load and format serodata of Sciensano
-df_sero = pd.read_csv('../../data/raw/sero/Belgium COVID-19 Studies - Sciensano_Blood Donors_Tijdreeks.csv', parse_dates=True)
-df_sero.index = df_sero['Date']
-df_sero.index = pd.to_datetime(df_sero.index)
-df_sero = df_sero.drop(columns=['Date'])
-df_sero['mean'] = df_sero['mean']*sum(initN) 
-df_sero_sciensano = df_sero
+# Serological data
+df_sero_herzog, df_sero_sciensano = sciensano.get_serological_data()
 
 # ------------------------
 # Define results locations
@@ -177,13 +180,40 @@ def add_poisson(state_name, output, n_samples, n_draws_per_sample, UL=1-0.05*0.5
 # ------------------------
 
 def draw_fcn(param_dict,samples_dict):
+
+    # Calibration of WAVE 1
+    # ---------------------
     idx, param_dict['beta'] = random.choice(list(enumerate(samples_dict['beta'])))
     param_dict['da'] = samples_dict['da'][idx]
     param_dict['l'] = samples_dict['l'][idx] 
     param_dict['prev_home'] = samples_dict['prev_home'][idx]      
     param_dict['prev_work'] = samples_dict['prev_work'][idx]       
     param_dict['prev_rest'] = samples_dict['prev_rest'][idx]
-    param_dict['zeta'] = samples_dict['zeta'][idx]      
+    param_dict['zeta'] = samples_dict['zeta'][idx]
+
+    # Hospitalization
+    # ---------------
+    # Fractions
+    names = ['c','m_C','m_ICU']
+    for idx,name in enumerate(names):
+        par=[]
+        for jdx in range(9):
+            par.append(np.random.choice(samples_dict['samples_fractions'][idx,jdx,:]))
+        param_dict[name] = np.array(par)
+    # Residence times
+    n=30
+    distributions = [samples_dict['residence_times']['dC_R'],
+                     samples_dict['residence_times']['dC_D'],
+                     samples_dict['residence_times']['dICU_R'],
+                     samples_dict['residence_times']['dICU_D']]
+    names = ['dc_R', 'dc_D', 'dICU_R', 'dICU_D']
+    for idx,dist in enumerate(distributions):
+        param_val=[]
+        for age_group in dist.index.get_level_values(0).unique().values[0:-1]:
+            draw = np.random.gamma(dist['shape'].loc[age_group],scale=dist['scale'].loc[age_group],size=n)
+            param_val.append(np.mean(draw))
+        param_dict[names[idx]] = np.array(param_val)
+
     return param_dict
 
 # --------------------
@@ -206,7 +236,7 @@ model = models.COVID19_SEIRD(initial_states, params,
 
 print('\n1) Simulating COVID-19 SEIRD '+str(args.n_samples)+' times')
 start_sim = '2020-03-10'
-end_sim = '2020-09-15'
+end_sim = '2020-09-03'
 out = model.sim(end_sim,start_date=start_calibration,warmup=warmup,N=args.n_samples,draw_fcn=draw_fcn,samples=samples_dict)
 
 # -----------
@@ -230,11 +260,10 @@ ax1.set_ylabel('$H_{in}$ (-)')
 # Plot fraction of immunes
 mean, median, LL, UL = add_poisson('R', out, args.n_samples, args.n_draws_per_sample)
 ax2.plot(out['time'],mean/sum(initN)*100,'--', color='blue')
-
-yerr = np.array([df_sero_herzog['mean'].values/sum(initN)*100 - df_sero_herzog['LL'].values*100, df_sero_herzog['UL'].values*100 - df_sero_herzog['mean'].values/sum(initN)*100 ])
-ax2.errorbar(x=df_sero_herzog.index,y=df_sero_herzog['mean']/sum(initN)*100,yerr=yerr, fmt='x', color='black', ecolor='gray', elinewidth=3, capsize=0)
-yerr = np.array([df_sero_sciensano['mean'].values/sum(initN)*100 - df_sero_sciensano['LL'].values*100, df_sero_sciensano['UL'].values*100 - df_sero_sciensano['mean'].values/sum(initN)*100 ])
-ax2.errorbar(x=df_sero_sciensano.index,y=df_sero_sciensano['mean'].values/sum(initN)*100,yerr=yerr, fmt='^', color='black', ecolor='gray', elinewidth=3, capsize=0)
+yerr = np.array([df_sero_herzog['rel','mean']*100 - df_sero_herzog['rel','LL']*100, df_sero_herzog['rel','UL']*100 - df_sero_herzog['rel','mean']*100 ])
+ax2.errorbar(x=df_sero_herzog.index,y=df_sero_herzog['rel','mean'].values*100,yerr=yerr, fmt='x', color='black', elinewidth=1, capsize=5)
+yerr = np.array([df_sero_sciensano['rel','mean']*100 - df_sero_sciensano['rel','LL']*100, df_sero_sciensano['rel','UL']*100 - df_sero_sciensano['rel','mean']*100 ])
+ax2.errorbar(x=df_sero_sciensano.index,y=df_sero_sciensano['rel','mean']*100,yerr=yerr, fmt='^', color='black', elinewidth=1, capsize=5)
 ax2 = _apply_tick_locator(ax2)
 ax2.legend(['model mean', 'Herzog et al. 2020', 'Sciensano'], bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=13)
 ax2.axvspan(end_calibration, '2021-12-31', facecolor='0.2', alpha=0.15)
@@ -246,7 +275,7 @@ ax2.set_ylabel('Seroprelevance (%)')
 plt.tight_layout()
 plt.show()
 
-print('3) Visualizing resusceptibility samples \n')
+print('3) Visualizing resusceptibility samples')
 
 fig,ax = plt.subplots(figsize=(12,4))
 data = 1/np.array(samples_dict['zeta'])/31
@@ -262,7 +291,73 @@ ax.spines['left'].set_visible(False)
 plt.tight_layout()
 plt.show()
 
-print('4) Saving model states on 2020-09-01 \n')
+print('\n4) Visualizing fit on hospitals')
+
+fig,(ax1,ax2,ax3,ax4) = plt.subplots(nrows=4,ncols=1,figsize=(12,16),sharex=True)
+
+simtime = pd.to_datetime(out['time'].values)
+mean, median, LL, UL = add_poisson('H_in', out, args.n_samples, args.n_draws_per_sample)
+ax1.plot(simtime, mean,'--', color='blue', linewidth=1)
+ax1.fill_between(simtime, LL, UL, alpha=0.20, color = 'blue')
+ax1.scatter(df_sciensano[start_calibration:end_calibration].index,df_sciensano['H_in'][start_calibration:end_calibration], color='black', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+ax1.scatter(df_sciensano[pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim].index,df_sciensano['H_in'][pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim], color='red', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+
+ax1 = _apply_tick_locator(ax1)
+ax1.set_ylabel('$H_{in}$ (-)')
+
+mean, median, LL, UL = add_poisson('H_tot', out, args.n_samples, args.n_draws_per_sample)
+ax2.plot(simtime, mean,'--', color='blue', linewidth=1)
+ax2.fill_between(simtime, LL, UL, alpha=0.20, color = 'blue')
+ax2.scatter(df_sciensano[start_calibration:end_calibration].index,df_sciensano['H_tot'][start_calibration:end_calibration], color='black', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+ax2.scatter(df_sciensano[pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim].index,df_sciensano['H_tot'][pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim], color='red', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+
+ax2 = _apply_tick_locator(ax2)
+ax2.set_ylabel('$H_{tot}$ (-)')
+
+mean, median, LL, UL = add_poisson('ICU', out, args.n_samples, args.n_draws_per_sample)
+ax3.plot(simtime, mean,'--', color='blue', linewidth=1)
+ax3.fill_between(simtime, LL, UL, alpha=0.20, color = 'blue')
+ax3.scatter(df_sciensano[start_calibration:end_calibration].index,df_sciensano['ICU_tot'][start_calibration:end_calibration], color='black', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+ax3.scatter(df_sciensano[pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim].index,df_sciensano['ICU_tot'][pd.to_datetime(end_calibration)+datetime.timedelta(days=1):end_sim], color='red', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+
+ax3 = _apply_tick_locator(ax3)
+ax3.set_ylabel('$ICU_{tot}$ (-)')
+
+mean, median, LL, UL = add_poisson('D', out, args.n_samples, args.n_draws_per_sample)
+ax4.plot(simtime, mean,'--', color='blue', linewidth=1)
+ax4.fill_between(simtime, LL, UL, alpha=0.20, color = 'blue')
+ax4.scatter(deaths_hospital[start_calibration:end_calibration].index,deaths_hospital[start_calibration:end_calibration], color='black', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+ax4.scatter(deaths_hospital[end_calibration:end_sim].index,deaths_hospital[end_calibration:end_sim], color='red', alpha=0.4, linestyle='None', facecolors='none', s=60, linewidth=2)
+
+ax4 = _apply_tick_locator(ax4)
+ax4.set_xlim('2020-03-01',end_sim)
+ax4.set_ylabel('$D_{tot}$ (-)')
+plt.show()
+
+print('5) Visualizing fit on deaths')
+
+dates = ['2020-05-01','2020-07-01','2020-09-01']
+
+fig,axes = plt.subplots(nrows=len(dates),ncols=1,figsize=(12,4*len(dates)),sharex=True)
+for idx,date in enumerate(dates):
+    data_sciensano = []
+    for jdx,age_group in enumerate(df_sciensano_mortality.index.get_level_values(0).unique().values):
+        data_sciensano.append(df_sciensano_mortality.xs(key=age_group, level="age_class", drop_level=True).loc[dates[idx],'cumsum_hospital'])
+    
+    axes[idx].scatter(df_sciensano_mortality.index.get_level_values(0).unique().values,out['D'].mean(dim='draws').loc[dict(time=date)],color='black',marker='v',zorder=1)
+    yerr = np.zeros([2,len(out['D'].quantile(dim='draws',q=0.975).loc[dict(time=date)].values)])
+    yerr[0,:] = out['D'].mean(dim='draws').loc[dict(time=date)] - out['D'].quantile(dim='draws',q=0.025).loc[dict(time=date)].values
+    yerr[1,:] = out['D'].quantile(dim='draws',q=0.975).loc[dict(time=date)].values - out['D'].mean(dim='draws').loc[dict(time=date)]
+    axes[idx].errorbar(x=df_sciensano_mortality.index.get_level_values(0).unique().values,
+                       y=out['D'].mean(dim='draws').loc[dict(time=date)],
+                       yerr=yerr,
+                       color = 'black', fmt = '--v', zorder=1, linewidth=1, ecolor='black', elinewidth=1, capsize=5)
+    axes[idx].bar(df_sciensano_mortality.index.get_level_values(0).unique().values,data_sciensano,width=1,alpha=0.7,zorder=0)
+    axes[idx].set_title('Cumulative hospital deaths on '+date)
+    axes[idx].grid(False)
+plt.show()
+
+print('6) Saving model states on 2020-09-01 \n')
 
 initial_states = {}
 for state in list(out.data_vars.keys()):
