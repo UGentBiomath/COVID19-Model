576399fb981815a1522b500be3450a0c68e64d03
Michiel Rollier <Michiel.Rollier@UGent.be>
2021-05-21 12:56:33 +0200

    Alter bash script for HPC run (constantly changed)

--- hpc/run-calibration-WAVE1_spatial.sh
+++ hpc/run-calibration-WAVE1_spatial.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N calibration-COVID19-SEIRD-WAVE1 ## job name
+#PBS -N calibration-COVID19-SEIRD-WAVE1_spatial ## job name
 #PBS -l nodes=1:ppn=36 ## single-node job, on 36 cores
 #PBS -l walltime=72:00:00 ## max. 72h of wall time
 
@@ -7,7 +7,7 @@ job="R0"
 warmup=0
 enddate=""
 maxiter=100
-number=10000
+number=100
 
 agg="arr"
 init="data"
@@ -15,11 +15,20 @@ indexpatients=3
 
 signature="job-${job}_${maxiter}xPSO_${number}xMCMC_${agg}_${indexpatients}-index-in-${init}"
 
+# Print job properties at the head of the stdout
+echo "job: ${job}"
+echo "warmup: ${warmup}"
+echo "enddate: ${enddate}"
+echo "PSO iterations: ${maxiter}"
+echo "MCMC iterations: ${number}"
+echo "spatial aggregation: ${agg}"
+echo "index patient spread: ${init}"
+echo "index patient number: ${indexpatients}"
+echo ""
 echo "Initiating job with signature ${signature}."
 
-
 # Change to package folder
-cd $VSC_HOME/Documents/COVID19-Model/hpc/
+cd $VSC_SCRATCH/Documents/COVID19-Model/hpc/
 
 # Make script executable
 chmod +x mrollier-calibration-WAVE1_spatial.py
@@ -27,8 +36,8 @@ chmod +x mrollier-calibration-WAVE1_spatial.py
 # Activate conda environment
 source activate COVID_MODEL
 
-# Execute script
-python mrollier-calibration-WAVE1_spatial.py -j ${job} -m ${maxiter} -n ${number} -a ${agg} -i ${init} -p ${indexpatients} -s ${signature}
+# Execute script. Note the python option -u to flush the prints to the stdout
+python -u mrollier-calibration-WAVE1_spatial.py -j ${job} -m ${maxiter} -n ${number} -a ${agg} -i ${init} -p ${indexpatients} -s ${signature}
 
 # Deactivate environment
 conda deactivate
