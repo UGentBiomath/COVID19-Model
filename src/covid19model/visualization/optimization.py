d73abb055485dcbe20822d2158c48a8b711c9f91
Michiel Rollier <Michiel.Rollier@UGent.be>
2021-06-04 16:16:50 +0200

    Fix issue with pd.Timestamp in xlim

--- src/covid19model/visualization/optimization.py
+++ src/covid19model/visualization/optimization.py
@@ -334,7 +334,7 @@ def plot_calibration_fit(out, df_sciensano, state, start_date, end_date, start_c
             ax.scatter(df_sciensano[start_calibration:end_calibration].index, df_sciensano[state][start_calibration:end_calibration], color='black', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
             ax.scatter(df_sciensano[end_calibration:end_date].index, df_sciensano[state][end_calibration:end_date], color='red', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
         ax = _apply_tick_locator(ax)
-        ax.set_xlim(start_date,end_date)
+        ax.set_xlim(pd.Timestamp(start_date)-pd.to_timedelta(1, unit='days'),pd.Timestamp(end_date)+pd.to_timedelta(1,'days'))
         ax.set_ylabel('$H_{in}$ (-)') # Hard-coded
         if savename:
             fig.savefig(savename, dpi=400, bbox_inches='tight')
@@ -351,7 +351,7 @@ def plot_calibration_fit(out, df_sciensano, state, start_date, end_date, start_c
                 ax.scatter(df_sciensano[start_calibration:end_calibration].index, df_sciensano.sum(axis=1)[start_calibration:end_calibration], color='black', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
                 ax.scatter(df_sciensano[end_calibration:end_date].index, df_sciensano.sum(axis=1)[end_calibration:end_date], color='red', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
             ax = _apply_tick_locator(ax)
-            ax.set_xlim(start_date,end_date)
+            ax.set_xlim(pd.Timestamp(start_date)-pd.to_timedelta(1, unit='days'),pd.Timestamp(end_date)+pd.to_timedelta(1,'days'))
             ax.set_ylabel('$H_{in}$ (national)') # Hard-coded
             if savename:
                 fig.savefig(savename, dpi=400, bbox_inches='tight')
@@ -367,7 +367,7 @@ def plot_calibration_fit(out, df_sciensano, state, start_date, end_date, start_c
                 ax.scatter(df_sciensano[start_calibration:end_calibration].index, df_sciensano[NIS][start_calibration:end_calibration], color='black', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
                 ax.scatter(df_sciensano[end_calibration:end_date].index, df_sciensano[NIS][end_calibration:end_date], color='red', alpha=0.6, linestyle='None', facecolors='none', s=60, linewidth=2)
             ax = _apply_tick_locator(ax)
-            ax.set_xlim(start_date,end_date)
+            ax.set_xlim(pd.Timestamp(start_date)-pd.to_timedelta(1, unit='days'),pd.Timestamp(end_date)+pd.to_timedelta(1,'days'))
             ax.set_ylabel('$H_{in}$ (NIS ' + str(NIS) + ')') # Hard-coded
             if savename:
                 fig.savefig(savename, dpi=400, bbox_inches='tight')
