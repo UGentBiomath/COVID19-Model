c633071e4c262b0f0c4f1420a2b691153cb74bb7
Michiel Rollier <Michiel.Rollier@UGent.be>
2021-06-15 12:05:12 +0200

    Add number of days in runtime print output

--- hpc/mrollier-calibration-WAVE1_spatial.py
+++ hpc/mrollier-calibration-WAVE1_spatial.py
@@ -362,7 +362,11 @@ if job == 'R0':
     runtime = (intermediate_time - initial_time)
     totalMinute, second = divmod(runtime.seconds, 60)
     hour, minute = divmod(totalMinute, 60)
-    print(f"Run time PSO: {hour}h{minute:02}m{second:02}s")
+    day = runtime.days
+    if day == 0:
+        print(f"Run time PSO: {hour}h{minute:02}m{second:02}s")
+    else:
+        print(f"Run time PSO: {day}d{hour}h{minute:02}m{second:02}s")
     sys.stdout.flush()
 
 
@@ -427,7 +431,11 @@ if job == 'R0':
     runtime = (final_time - intermediate_time)
     totalMinute, second = divmod(runtime.seconds, 60)
     hour, minute = divmod(totalMinute, 60)
-    print(f"Run time MCMC: {hour}h{minute:02}m{second:02}s")
+    day = runtime.days
+    if day == 0:
+        print(f"Run time MCMC: {hour}h{minute:02}m{second:02}s")
+    else:
+        print(f"Run time MCMC: {day}d{hour}h{minute:02}m{second:02}s")
     sys.stdout.flush()
 
     print('\n3) Sending samples to dictionary')
@@ -560,7 +568,11 @@ elif job == 'FULL':
     runtime = (intermediate_time - initial_time)
     totalMinute, second = divmod(runtime.seconds, 60)
     hour, minute = divmod(totalMinute, 60)
-    print(f"Run time PSO: {hour}h{minute:02}m{second:02}s")
+    day = runtime.days
+    if day == 0:
+        print(f"Run time PSO: {hour}h{minute:02}m{second:02}s")
+    else:
+        print(f"Run time PSO: {day}d{hour}h{minute:02}m{second:02}s")
     sys.stdout.flush()
 
 
@@ -630,7 +642,11 @@ elif job == 'FULL':
     runtime = (final_time - intermediate_time)
     totalMinute, second = divmod(runtime.seconds, 60)
     hour, minute = divmod(totalMinute, 60)
-    print(f"Run time MCMC: {hour}h{minute:02}m{second:02}s")
+    day = runtime.days
+    if day == 0:
+        print(f"Run time MCMC: {hour}h{minute:02}m{second:02}s")
+    else:
+        print(f"Run time MCMC: {day}d{hour}h{minute:02}m{second:02}s")
     sys.stdout.flush()
 
     print('\n3) Sending samples to dictionary')
