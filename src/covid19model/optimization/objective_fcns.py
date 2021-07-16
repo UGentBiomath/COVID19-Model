f4525fe9134d10dece6b78d6b4929cb195ac0f37
twallema <tijs.alleman@ugent.be>
2021-07-16 15:39:03 +0200

    tryout notebook SAlib

--- src/covid19model/optimization/objective_fcns.py
+++ src/covid19model/optimization/objective_fcns.py
@@ -44,7 +44,7 @@ def MLE(thetas,model,data,states,parNames,weights=[1],draw_fcn=None,samples=None
     -----------
     MLE = MLE(model,thetas,data,parNames,positions)
     """
-
+    
     # ~~~~~~~~~~~~~~~~~~~~
     # perform input checks
     # ~~~~~~~~~~~~~~~~~~~~ 
@@ -132,7 +132,7 @@ def MLE(thetas,model,data,states,parNames,weights=[1],draw_fcn=None,samples=None
                     ymodel = out[state].sel(place=NIS).sum(dim="Nc").sel(time=data[idx].index.values, method='nearest').values
                     MLE_add = weights[idx]*ll_poisson(ymodel, data[idx][NIS], offset=poisson_offset)
                     MLE += MLE_add # multiplication of likelihood is sum of loglikelihoods
-    
+
     return -MLE # must be positive for pso, which attempts to minimises MLE
 
 def ll_gaussian(ymodel, ydata, sigma):
