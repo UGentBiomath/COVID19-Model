bbe50175d08df7b3da584b1100c39aa91d900e28
twallema <tijs.alleman@ugent.be>
2021-08-24 14:01:43 +0200

    Experiment with different ensemble moves

--- src/covid19model/optimization/utils.py
+++ src/covid19model/optimization/utils.py
@@ -37,8 +37,9 @@ def run_MCMC(pos, max_n, print_n, labels, objective_fcn, objective_fcn_args, obj
 
     with Pool() as pool:
         sampler = emcee.EnsembleSampler(nwalkers, ndim, objective_fcn, backend=backend, pool=pool,
-                        args=objective_fcn_args, kwargs=objective_fcn_kwargs)
-        for sample in sampler.sample(pos, iterations=max_n, progress=progress, store=True):
+                        args=objective_fcn_args, kwargs=objective_fcn_kwargs,
+                        moves=[(emcee.moves.DEMove(), 0.8),(emcee.moves.DESnookerMove(), 0.2)])
+        for sample in sampler.sample(pos, iterations=max_n, progress=progress, store=True, tune=True):
             # Only check convergence every print_n steps
             if sampler.iteration % print_n:
                 continue
