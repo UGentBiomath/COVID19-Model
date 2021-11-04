3145b41618e5ecc7cb4920ec0d32295c425d97fa
twallema <tijs.alleman@ugent.be>
2021-11-04 16:23:06 +0100

    tried setting tune to False (didn't work)

--- src/covid19model/optimization/utils.py
+++ src/covid19model/optimization/utils.py
@@ -39,7 +39,7 @@ def run_MCMC(pos, max_n, print_n, labels, objective_fcn, objective_fcn_args, obj
         sampler = emcee.EnsembleSampler(nwalkers, ndim, objective_fcn, backend=backend, pool=pool,
                         args=objective_fcn_args, kwargs=objective_fcn_kwargs,
                         moves=[(emcee.moves.DEMove(), 0.8),(emcee.moves.DESnookerMove(), 0.2)])
-        for sample in sampler.sample(pos, iterations=max_n, progress=progress, store=True, tune=True):
+        for sample in sampler.sample(pos, iterations=max_n, progress=progress, store=True, tune=False):
             # Only check convergence every print_n steps
             if sampler.iteration % print_n:
                 continue
