f8e132eac71a4900b2b845a6da7e6a74f6a132ec
twallema <tijs.alleman@ugent.be>
2021-11-05 11:36:25 +0100

    comment out superfluous parameters in model.parameters

--- src/covid19model/data/model_parameters.py
+++ src/covid19model/data/model_parameters.py
@@ -425,12 +425,12 @@ def get_COVID19_SEIQRD_parameters(age_stratification_size=10, spatial=None, vacc
         pars_dict['place'] = NIS
 
         # Read areas per region, ordered in ascending NIS values
-        area_data = '../../../data/interim/demographic/area_' + spatial + '.csv'
-        area_df=pd.read_csv(os.path.join(abs_dir, area_data), index_col='NIS')
+        #area_data = '../../../data/interim/demographic/area_' + spatial + '.csv'
+        #area_df=pd.read_csv(os.path.join(abs_dir, area_data), index_col='NIS')
         # Make sure the regions are ordered well
-        area_df=area_df.sort_index(axis=0)
-        area=area_df.values[:,0]
-        pars_dict['area'] = area * 1e-6 # in square kilometer
+        #area_df=area_df.sort_index(axis=0)
+        #area=area_df.values[:,0]
+        #pars_dict['area'] = area * 1e-6 # in square kilometer
 
         # Load mobility parameter, which is regionally stratified and 1 by default (no user-defined mobility changes)
         p = np.ones(pars_dict['place'].shape[0])
@@ -438,11 +438,11 @@ def get_COVID19_SEIQRD_parameters(age_stratification_size=10, spatial=None, vacc
 
         # Load average household size sigma_g (sg) per region. Set default to average 2.3 for now.
         # NB Currently not used
-        sg = np.ones(pars_dict['place'].shape[0]) * 2.3
-        pars_dict['sg'] = sg
+        #sg = np.ones(pars_dict['place'].shape[0]) * 2.3
+        #pars_dict['sg'] = sg
 
         # Define factor controlling the contact dependence on density f (hard-coded)
-        xi = 0.01 # km^-2
-        pars_dict['xi'] = xi
+        #xi = 0.01 # km^-2
+        #pars_dict['xi'] = xi
 
     return initN, Nc_dict, pars_dict
