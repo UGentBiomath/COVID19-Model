cc5026fc4a980befdca1007035841c8eb80eed53
Tijs Alleman <tijs.alleman@ugent.be>
2022-03-30 13:17:51 +0200

    update draw function

--- src/covid19model/models/utils.py
+++ src/covid19model/models/utils.py
@@ -726,13 +726,12 @@ def draw_fcn_COVID19_SEIQRD_spatial(param_dict,samples_dict):
     idx, param_dict['beta_R'] = random.choice(list(enumerate(samples_dict['beta_R'])))
     param_dict['beta_U'] = samples_dict['beta_U'][idx]  
     param_dict['beta_M'] = samples_dict['beta_M'][idx]
-    param_dict['l1'] = samples_dict['l1'][idx]  
-    param_dict['l2'] = samples_dict['l2'][idx]  
     param_dict['eff_schools'] = samples_dict['eff_schools'][idx]    
     param_dict['eff_home'] = samples_dict['eff_home'][idx]      
     param_dict['eff_work'] = samples_dict['eff_work'][idx]       
     param_dict['eff_rest'] = samples_dict['eff_rest'][idx]
     param_dict['mentality'] = samples_dict['mentality'][idx]
+    param_dict['K_inf'] = np.array([samples_dict['K_inf_abc'][idx], samples_dict['K_inf_delta'][idx]], np.float64)
     param_dict['amplitude'] = samples_dict['amplitude'][idx]
     param_dict['zeta'] = samples_dict['zeta'][idx]
 
