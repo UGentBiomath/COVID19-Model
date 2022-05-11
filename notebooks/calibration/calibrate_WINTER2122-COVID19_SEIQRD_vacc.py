eeaba89be41f5b9505660546f5aae2a48d1c4ff3
Tijs Alleman <tijs.alleman@ugent.be>
2022-05-11 12:04:56 +0200

    winter calibration script

--- notebooks/calibration/calibrate_WINTER2122-COVID19_SEIQRD_vacc.py
+++ notebooks/calibration/calibrate_WINTER2122-COVID19_SEIQRD_vacc.py
@@ -147,12 +147,12 @@ if __name__ == '__main__':
     ##########################
 
     # PSO settings
-    processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count())/2-1)
+    processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count())/2)
     multiplier_pso = 10
     maxiter = n_pso
     popsize = multiplier_pso*processes
     # MCMC settings
-    multiplier_mcmc = 4
+    multiplier_mcmc = 10
     max_n = n_mcmc
     print_n = 10
     # Define dataset
@@ -182,17 +182,18 @@ if __name__ == '__main__':
     bounds2=((0.01,0.99),)
     # Omicron infectivity
     pars3 = ['K_inf',]
-    bounds3 = ((1.2,2.40),)
+    bounds3 = ((1.3,2.60),)
     # Omicron severity
     pars4 = ['K_hosp',]
-    bounds4 = ((0.05,0.50),)
+    bounds4 = ((0.05,0.70),)
     # Join them together
     pars = pars1 + pars2 + pars3 + pars4
     bounds = bounds1 + bounds2 + bounds3 + bounds4
     # run optimization
     #theta = fit_pso(model, data, pars, states, bounds, weights, maxiter=maxiter, popsize=popsize,
     #                    start_date=start_calibration, warmup=warmup, processes=processes)
-    theta = np.array([0.058, 0.5, 2.2, 0.08]) # Good starting estimate for "stratified" vaccination
+    theta = np.array([0.058, 0.5, 2.2, 0.08]) # Starting estimated based on calibration until 2022-03-01
+    theta = np.array([0.0592, 0.462, 1.81, 0.28]) # Starting estimate based on calibration until 2022-01-15
 
     ####################################
     ## Local Nelder-mead optimization ##
@@ -257,13 +258,13 @@ if __name__ == '__main__':
     log_prior_fnc_args = bounds
     # Perturbate PSO Estimate
     # pars1 = ['beta',]
-    pert1=[0.05,]
+    pert1=[0.03,]
     # pars2 = ['mentality',]
-    pert2=[0.30,]
+    pert2=[0.20,]
     # pars4 = ['K_inf',]
     pert3=[0.30,]
     # pars5 = ['K_hosp']
-    pert4 = [0.30,] 
+    pert4 = [0.95,] 
     # Add them together and perturbate
     pert = pert1 + pert2 + pert3 + pert4
     # Labels for traceplots
