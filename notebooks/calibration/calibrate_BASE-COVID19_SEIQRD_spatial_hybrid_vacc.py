a8eff0c2ab19405a1addc4223b6f2d820ae29a71
Tijs Alleman <tijs.alleman@ugent.be>
2022-10-10 10:42:13 +0200

    calibration script

--- notebooks/calibration/calibrate_BASE-COVID19_SEIQRD_spatial_hybrid_vacc.py
+++ notebooks/calibration/calibrate_BASE-COVID19_SEIQRD_spatial_hybrid_vacc.py
@@ -165,15 +165,15 @@ if __name__ == '__main__':
     maxiter = n_pso
     popsize = multiplier_pso*processes
     # MCMC settings
-    multiplier_mcmc = 10
+    multiplier_mcmc = 5
     max_n = n_mcmc
     print_n = 10
     # Define dataset
     data=[df_hosp.loc[(slice(start_calibration,end_calibration), slice(None)), 'H_in'], df_sero_herzog['abs','mean'], df_sero_sciensano['abs','mean'][:23]]
     states = ["H_in", "R", "R"]
     weights = np.array([1, 1, 1]) # Scores of individual contributions: 1) 17055, 2+3) 255 860, 3) 175571
-    log_likelihood_fnc = [ll_negative_binomial, ll_negative_binomial, ll_negative_binomial]
-    log_likelihood_fnc_args = [results.loc[(slice(None), 'negative binomial'), 'theta'].values, dispersion_weighted, dispersion_weighted]
+    log_likelihood_fnc = [ll_poisson, ll_negative_binomial, ll_negative_binomial]
+    log_likelihood_fnc_args = [[],[dispersion_weighted],[dispersion_weighted]] #[results.loc[(slice(None), 'negative binomial'), 'theta'].values, dispersion_weighted, dispersion_weighted]
 
     print('\n--------------------------------------------------------------------------------------')
     print('PERFORMING CALIBRATION OF INFECTIVITY, COMPLIANCE, CONTACT EFFECTIVITY AND SEASONALITY')
@@ -192,14 +192,14 @@ if __name__ == '__main__':
     bounds1=((0.01,0.070),(0.01,0.070),(0.01,0.070))
     # Social intertia
     # Effectivity parameters
-    pars2 = ['eff_schools', 'eff_work', 'eff_rest', 'mentality']
-    bounds2=((0,2),(0,2),(0,2),(0,2))
+    pars2 = ['eff_work', 'eff_rest', 'mentality']
+    bounds2=((0,0.80),(0,1),(0,1))
     # Variants
     pars3 = ['K_inf',]
-    bounds3 = ((1.20, 1.45),(1.45,2.20))
+    bounds3 = ((1.20, 1.60),(1.50,2.20))
     # Seasonality
     pars4 = ['amplitude',]
-    bounds4 = ((0,0.50),)
+    bounds4 = ((0,0.40),)
     # Join them together
     pars = pars1 + pars2 + pars3 + pars4  
     bounds = bounds1 + bounds2 + bounds3 + bounds4
@@ -210,16 +210,16 @@ if __name__ == '__main__':
     #                                                                        swarmsize=popsize, maxiter=maxiter, processes=processes, debug=True)
 
     theta = [0.023, 0.0232, 0.0273, 0.375, 0.375, 0.799, 0.522, 1.34, 1.45, 0.24] # Obtained from MCMC run on 2022-09-16
-    theta = [0.023, 0.0232, 0.0285, 0.6, 0.4, 0.7, 0.522, 1.3, 1.45, 0.24]
+    theta =  [0.023, 0.0232, 0.026, 0.5, 0.65, 0.522, 1.35, 1.45, 0.24] # --> mentality: option 2, eff_schools and eff_work together
 
     ####################################
     ## Local Nelder-mead optimization ##
     ####################################
     
     # Define objective function
-    #objective_function = log_posterior_probability([],[],model,pars,data,states,log_likelihood_fnc,log_likelihood_fnc_args,-weights)
+    objective_function = log_posterior_probability([],[],model,pars,data,states,log_likelihood_fnc,log_likelihood_fnc_args,-weights)
     # Run Nelder Mead optimization
-    step = len(bounds)*[0.05,]
+    #step = len(bounds)*[0.05,]
     #sol = nelder_mead(objective_function, np.array(theta), step, (), processes=processes)
 
     #######################################
@@ -302,21 +302,21 @@ if __name__ == '__main__':
     # Perturbate PSO estimate by a certain maximal *fraction* in order to start every chain with a different initial condition
     # Generally, the less certain we are of a value, the higher the perturbation fraction
     # pars1 = ['beta_R', 'beta_U', 'beta_M']
-    pert1=[0.01, 0.01, 0.01]
-    # pars2 = ['eff_schools', 'eff_work', 'eff_rest', 'mentality', 'eff_home']
-    pert2=[0.50, 0.50, 0.50, 0.50]
+    pert1=[0.02, 0.02, 0.02]
+    # pars2 = ['eff_work', 'eff_rest', 'mentality']
+    pert2=[0.05, 0.05, 0.05]
     # pars3 = ['K_inf_abc', 'K_inf_delta']
     pert3 = [0.05, 0.05]
     # pars4 = ['amplitude']
-    pert4 = [0.50,] 
+    pert4 = [0.05,] 
     # Add them together
     pert = pert1 + pert2 + pert3 + pert4
     # Labels for traceplots
     labels = ['$\\beta_R$', '$\\beta_U$', '$\\beta_M$', \
-                '$\\Omega_{schools}$', '$\\Omega_{work}$', '$\\Omega_{rest}$', 'M', \
+                '$\\Omega_{work}$', '$\\Omega_{rest}$', 'M', \
                 '$K_{inf, abc}$', '$K_{inf, delta}$', \
                 '$A$']
-    pars_postprocessing = ['beta_R', 'beta_U', 'beta_M', 'eff_schools', 'eff_work', 'eff_rest', 'mentality', 'K_inf_abc', 'K_inf_delta', 'amplitude']
+    pars_postprocessing = ['beta_R', 'beta_U', 'beta_M', 'eff_work', 'eff_rest', 'mentality', 'K_inf_abc', 'K_inf_delta', 'amplitude']
     # Use perturbation function
     ndim, nwalkers, pos = perturbate_PSO(theta, pert, multiplier=multiplier_mcmc, bounds=log_prior_fnc_args, verbose=False)
     # Set up the sampler backend if needed
