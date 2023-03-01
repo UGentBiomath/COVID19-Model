0c0be37f21e8676a9b33670687faf2be9062a9ef
Tijs Alleman <tijs.alleman@ugent.be>
2023-03-01 14:19:44 +0100

    draw function spatial model

--- src/covid19model/models/utils.py
+++ src/covid19model/models/utils.py
@@ -466,7 +466,7 @@ def draw_fnc_COVID19_SEIQRD_spatial_hybrid_vacc(param_dict,samples_dict):
 
     idx, param_dict['beta_R'] = random.choice(list(enumerate(samples_dict['beta_R'])))
     param_dict['beta_U'] = samples_dict['beta_U'][idx]  
-    param_dict['beta_M'] = samples_dict['beta_M'][idx]    
+    param_dict['beta_M'] = samples_dict['beta_M'][idx]
     param_dict['eff_work'] = samples_dict['eff_work'][idx]
     param_dict['eff_rest'] = samples_dict['eff_rest'][idx]   
     param_dict['k'] = samples_dict['k'][idx]
