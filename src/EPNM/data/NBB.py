ab96142f93e3f9fdc4e9566fa21ea2edec6a44ee
Tijs Alleman <tijs.alleman@ugent.be>
2023-03-22 13:12:18 +0100

    make all data "absolute"

--- src/EPNM/data/NBB.py
+++ src/EPNM/data/NBB.py
@@ -1,13 +1,22 @@
 import os
+import numpy as np
 import pandas as pd
+from EPNM.data.parameters import get_model_parameters
+from EPNM.data.utils import get_sector_labels, get_sectoral_conversion_matrix
 
 # Set path to interim data folder
 abs_dir = os.path.dirname(__file__)
 par_interim_path = os.path.join(abs_dir, "../../../data/EPNM/interim/")
 
-def get_revenue_survey():
+def get_revenue_survey(relative=True):
     """
     Loads the revenue survey data located in `data/EPNM/interim/calibration_data/ERMG_revenue_survey.xlsx`
+
+    Parameters
+    ==========
+
+    relative: bool
+        Relative or absolute data.
 
     Returns
     =======
@@ -31,12 +40,27 @@ def get_revenue_survey():
     df = (100+df)/100
     df = df.swaplevel()
     df = df.groupby(by=['date', 'NACE64']).sum()
-
+    # Multiply with sectoral output 'x' as proxy for revenue
+    if relative == False:
+        parameters = get_model_parameters()
+        labels = get_sector_labels('NACE64')
+        for sector in df.index.get_level_values('NACE64').unique():
+            if sector == 'BE':
+                df.loc[slice(None), 'BE'] = df.loc[slice(None), 'BE'].values*np.sum(parameters['x_0'])
+            else:
+                i = labels.index(sector)
+                df.loc[slice(None), sector] = df.loc[slice(None), sector].values*parameters['x_0'][i]
     return df
 
-def get_employment_survey():
+def get_employment_survey(relative=True):
     """
     Loads the employment survey data located in `data/EPNM/interim/calibration_data/ERMG_temporary_unemployment.xlsx`
+
+    Parameters
+    ==========
+
+    relative: bool
+        Relative or absolute data.
 
     Returns
     =======
@@ -56,11 +80,27 @@ def get_employment_survey():
     df = (100-df)/100
     # Series name
     df.name = 'temporary_unemployment'
+    # Multiply with sectoral labor income 'l' as proxy
+    if relative == False:
+        parameters = get_model_parameters()
+        labels = get_sector_labels('NACE64')
+        for sector in df.index.get_level_values('NACE64').unique():
+            if sector == 'BE':
+                df.loc[slice(None), 'BE'] = df.loc[slice(None), 'BE'].values*np.sum(parameters['l_0'])
+            else:
+                i = labels.index(sector)
+                df.loc[slice(None), sector] = df.loc[slice(None), sector].values*parameters['l_0'][i]
     return df
 
-def get_synthetic_GDP():
+def get_synthetic_GDP(relative=True):
     """
     Loads the synthetic GDP data located in `data/EPNM/interim/calibration_data/NBB_synthetic_GDP.xlsx`
+
+    Parameters
+    ==========
+
+    relative: bool
+        Relative or absolute data.
 
     Returns
     =======
@@ -80,11 +120,27 @@ def get_synthetic_GDP():
     df = (100+df)/100
     # Series name
     df.name = 'synthetic_GDP'
+    # Multiply with sectoral output 'x' as proxy for revenue
+    if relative == False:
+        parameters = get_model_parameters()
+        labels = get_sector_labels('NACE64')
+        for sector in df.index.get_level_values('NACE64').unique():
+            if sector == 'BE':
+                df.loc[slice(None), 'BE'] = df.loc[slice(None), 'BE'].values*np.sum(parameters['x_0'])
+            else:
+                i = labels.index(sector)
+                df.loc[slice(None), sector] = df.loc[slice(None), sector].values*parameters['x_0'][i]
     return df
 
-def get_B2B_demand():
+def get_B2B_demand(relative=True):
     """
     Loads the B2B demand data located in `data/EPNM/interim/calibration_data/WoW_Growths.xlsx`
+
+    Parameters
+    ==========
+
+    relative: bool
+        Relative or absolute data.
 
     Returns
     =======
@@ -110,4 +166,13 @@ def get_B2B_demand():
     df.index = df.index.rename(['date', 'NACE21'])
     # Series name
     df.name = 'B2B demand'
+    # Multiply with orders 'O_j' as proxy for B2B demand
+    if relative == False:
+        parameters = get_model_parameters()
+        labels = get_sector_labels('NACE21')
+        O_j = np.matmul(get_sectoral_conversion_matrix('NACE38_NACE21'), np.matmul(get_sectoral_conversion_matrix('NACE64_NACE38'), parameters['O_j']))
+        for sector in df.index.get_level_values('NACE21').unique():
+            if sector != 'U':
+                i = labels.index(sector)
+                df.loc[slice(None), sector] = df.loc[slice(None), sector].values*O_j[i]
     return df
