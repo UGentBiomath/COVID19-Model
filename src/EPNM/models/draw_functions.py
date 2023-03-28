e82655dd1eb0339ccc11d507baef92680590281b
Tijs Alleman <tijs.alleman@ugent.be>
2023-03-28 17:21:07 +0200

    two options for draw functions

--- src/EPNM/models/draw_functions.py
+++ src/EPNM/models/draw_functions.py
@@ -8,22 +8,40 @@ def draw_function(param_dict, samples_dict):
     """
 
     # Hiring and Firing speed
-    param_dict['gamma_F'] = np.random.normal(loc=7, scale=1)
-    param_dict['gamma_H'] = np.random.normal(loc=14, scale=2)
+    #param_dict['gamma_F'] = np.random.normal(loc=7, scale=2)
+    #param_dict['gamma_H'] = np.random.normal(loc=28, scale=7)
+    # Household savings and prospects
+    #param_dict['delta_S'] = np.random.uniform(low=0, high=1)
+    #param_dict['L'] = np.random.uniform(low=0, high=1)
+    # restock rate
+    #param_dict['tau'] =  np.random.normal(loc=10, scale=2)
+    # Shocks lockdowns
+    #param_dict['l_s_1'] =  np.random.normal(loc=1, scale=0.05)*param_dict['l_s_1']
+    #param_dict['l_s_2'] =  np.random.normal(loc=1, scale=0.05)*param_dict['l_s_2']
+    #param_dict['c_s'] =  np.random.normal(loc=1, scale=0.05)*param_dict['c_s']
+    #param_dict['f_s'] =  np.random.normal(loc=1, scale=0.05)*param_dict['f_s']
+    # Shocks summer 2020 
+    #param_dict['ratio_c_s'] =  np.random.uniform(low=1/10, high=0.99)
+    #param_dict['ratio_f_s'] =  np.random.uniform(low=1/10, high=0.99)
+    
+    # Option 2
+    # Calibrated c_s and f_s
+    idx = np.random.randint(low=0, high=len(samples_dict['c_s'][0]))
+    param_dict['c_s'] = np.array([slice[idx] for slice in samples_dict['c_s']], np.float64)
+    param_dict['f_s'] = np.array([slice[idx] for slice in samples_dict['f_s']], np.float64)
+    # Perturbate l_s
+    param_dict['l_s_1'] =  np.random.normal(loc=1, scale=0.05)*param_dict['l_s_1']
+    param_dict['l_s_2'] =  np.random.normal(loc=1, scale=0.05)*param_dict['l_s_2']
+    # Perturbate ratio's during summer of 2020
+    param_dict['ratio_c_s'] =  np.random.uniform(low=1/10, high=0.99)
+    param_dict['ratio_f_s'] =  np.random.uniform(low=1/10, high=0.99)
+    # Hiring and Firing speed
+    param_dict['gamma_F'] = np.random.normal(loc=7, scale=2)
+    param_dict['gamma_H'] = np.random.normal(loc=28, scale=7)
     # Household savings and prospects
     param_dict['delta_S'] = np.random.uniform(low=0, high=1)
     param_dict['L'] = np.random.uniform(low=0, high=1)
     # restock rate
     param_dict['tau'] =  np.random.normal(loc=10, scale=2)
-    # Shocks lockdowns
-    param_dict['l_s_1'] =  np.random.normal(loc=1, scale=0.08)*param_dict['l_s_1']
-    param_dict['l_s_2'] =  np.random.normal(loc=1, scale=0.08)*param_dict['l_s_2']
-    param_dict['c_s'] =  np.random.normal(loc=1, scale=0.08)*param_dict['c_s']
-    param_dict['f_s'] =  np.random.normal(loc=1, scale=0.08)*param_dict['f_s']
-    # Shocks summer 2020 (calibrated)
-    # idx, param_dict['ratio_c_s'] = random.choice(list(enumerate(samples_dict['ratio_c_s'])))  
-    # param_dict['ratio_f_s'] = samples_dict['ratio_f_s'][idx]
-    param_dict['ratio_c_s'] =  np.random.uniform(low=1/10, high=0.99)
-    param_dict['ratio_f_s'] =  np.random.uniform(low=1/10, high=0.99)
-    
+
     return param_dict
