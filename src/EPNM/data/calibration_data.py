8e7d9ec9d1fa0057a47c7cf17d5d44caecac991d
Tijs Alleman <tijs.alleman@ugent.be>
2023-03-28 17:23:45 +0200

    add a function for the added value NAI data

--- src/EPNM/data/calibration_data.py
+++ src/EPNM/data/calibration_data.py
@@ -8,6 +8,66 @@ from EPNM.data.utils import get_sector_labels, get_sectoral_conversion_matrix
 abs_dir = os.path.dirname(__file__)
 par_interim_path = os.path.join(abs_dir, "../../../data/EPNM/interim/")
 
+def get_NAI_value_added(NACE10=True, relative=True):
+    """
+    Loads the NAI value added data located in `data/EPNM/interim/calibration_data/NAI_value_added.csv`
+    Source: https://www.nbb.be/doc/ts/publications/economicreview/2021/ecorevii2021.pdf
+
+    Parameters
+    ==========
+
+    relative: bool
+        Relative or absolute data.
+
+    Returns
+    =======
+
+    df: pd.Series
+        Contains the value added in services (NACE10) during the SARS-CoV-2 pandemic. Two index levels: dates and NACE10 sector
+    """
+    if NACE10==False:
+        # Get raw dataframe
+        df = pd.read_csv(os.path.join(par_interim_path,"calibration_data/NAI_value_added.csv"), header=[0],index_col=[1], parse_dates=True)
+        # Drop quarter column
+        df = df.drop(columns=['quarter'])
+        # Stack columns into multiindex
+        df = df.stack()
+        # Assign a name to the index levels
+        df.index = df.index.rename(['date', 'NACE10/NACE21'])
+        # Multiply with sectoral output 'x' as proxy for revenue
+        if relative == False:
+            parameters = get_model_parameters()
+            x_NACE10 = np.matmul(get_sectoral_conversion_matrix('NACE21_NACE10'), np.matmul(get_sectoral_conversion_matrix('NACE38_NACE21'), np.matmul(get_sectoral_conversion_matrix('NACE64_NACE38'), parameters['x_0'])))
+            x_NACE21 = np.matmul(get_sectoral_conversion_matrix('NACE38_NACE21'), np.matmul(get_sectoral_conversion_matrix('NACE64_NACE38'), parameters['x_0']))
+            labels = get_sector_labels('NACE10')
+            for sector in df.index.get_level_values('NACE10/NACE21').unique():
+                if ((sector != 'O-P') & (sector != 'Q')):
+                    i = labels.index(sector)
+                    df.loc[slice(None), sector] = df.loc[slice(None), sector].values*x_NACE10[i]
+            # O-P/Q manually (NACE10: O-P-Q)
+            labels = get_sector_labels('NACE21')
+            df.loc[slice(None), 'O-P'] = df.loc[slice(None), 'O-P'].values*(x_NACE21[labels.index('O')] + x_NACE21[labels.index('P')])
+            df.loc[slice(None), 'Q'] = df.loc[slice(None), 'Q'].values*x_NACE21[labels.index('Q')]
+    else:
+        # Get raw dataframe
+        df = pd.read_csv(os.path.join(par_interim_path,"calibration_data/NAI_value_added_NACE10.csv"), header=[0],index_col=[1], parse_dates=True)
+        # Drop quarter column
+        df = df.drop(columns=['quarter'])
+        # Stack columns into multiindex
+        df = df.stack()
+        # Assign a name to the index levels
+        df.index = df.index.rename(['date', 'NACE10'])
+        # Multiply with sectoral output 'x' as proxy for revenue
+        if relative == False:
+            parameters = get_model_parameters()
+            x_NACE10 = np.matmul(get_sectoral_conversion_matrix('NACE21_NACE10'), np.matmul(get_sectoral_conversion_matrix('NACE38_NACE21'), np.matmul(get_sectoral_conversion_matrix('NACE64_NACE38'), parameters['x_0'])))
+            labels = get_sector_labels('NACE10')
+            for sector in df.index.get_level_values('NACE10').unique():
+                i = labels.index(sector)
+                df.loc[slice(None), sector] = df.loc[slice(None), sector].values*x_NACE10[i]
+
+    return df
+
 def get_revenue_survey(relative=True):
     """
     Loads the revenue survey data located in `data/EPNM/interim/calibration_data/ERMG_revenue_survey.xlsx`
